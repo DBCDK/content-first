ad57500f224d08c9469e5188ef4adef15d1010a4
Kasper Fuglsang <kfu@dbc.dk>
2017-09-04 11:24:47 +0200

    added mouse over belt-title handling

--- src/client/components/frontpage/Belt.component.js
+++ src/client/components/frontpage/Belt.component.js
@@ -1,47 +1,72 @@
 import React from 'react';
 
-export default function Belt(props) {
-  const scrollPos = props.belt.scrollOffset ? (-1 * props.belt.scrollOffset * 275) + 'px' : '0px';
-  return (
-    <div className='row belt text-left'>
-      <div className='col-xs-12 header'>
-        <span className='belt-title'>{props.belt.name}</span>
-        {props.belt.works && props.belt.works.length > 5 &&
-          <span className='more-link btn' onClick={props.onMoreClick}>Se flere</span>}
-      </div>
-      <div className='col-xs-12 tags'>
-        {props.belt.tags && props.belt.tags.map((tag, idx) => {
-          const btnClass = tag.selected ? 'btn-success' : 'btn-default';
-          return <span className={`btn ${btnClass}`} key={idx} onClick={() => {
-            props.onTagClick(idx);
-          }}>{tag.name}</span>;
-        })}
-      </div>
-      {props.custom}
-      <div className='belt-wrapper'>
-        {props.belt.works && (
-          <div className='button-wrapper col-xs-12 noselect'>
-            <div className='left-btn scroll-btn text-center' onClick={props.onScrollLeft}>{'<'}</div>
-            <div className='right-btn scroll-btn text-center' onClick={props.onScrollRight}>{'>'}</div>
-          </div>
-        )}
-        <div className='works-wrapper col-xs-12 noselect'>
-          <div className='works' style={{transform: `translate3d(${scrollPos}, 0px, 0px)`}}>
-            {props.belt.works && props.belt.works.map((work, idx) => {
-              return (
-                <div className='work' id={`work-${idx}`} key={idx}>
-                  <div className='cover-image-wrapper'>
-                    <img alt="" className='cover-image' src={work.cover}/>
+export default class Belt extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {showDetails: false};
+  }
+
+  showDetails(show) {
+    if (this.showDetailsTimeout) {
+      clearTimeout(this.showDetailsTimeout);
+    }
+    this.showDetailsTimeout = setTimeout(() => {
+      this.setState({showDetails: show});
+    }, show ? 250 : 1000);
+  }
+
+  render() {
+    const scrollPos = this.props.belt.scrollOffset ? (-1 * this.props.belt.scrollOffset * 275) + 'px' : '0px';
+    const detailsClassName = this.state.showDetails ? 'belt-title-details' : 'belt-title-details no-opacity';
+    return (
+      <div className='row belt text-left'>
+        <div className='col-xs-12 header'>
+          <span className='belt-title'
+            onMouseOver={() => this.showDetails(true)}
+            onMouseOut={() => this.showDetails(false)}>
+            {this.props.belt.name}</span>
+          {this.props.belt.works && this.props.belt.works.length > 5 &&
+          <span className='more-link btn' onClick={this.props.onMoreClick}>Se flere</span>}
+          <div className={detailsClassName}
+            onMouseOver={() => this.showDetails(true)}
+            onMouseOut={() => this.showDetails(false)}>
+            Jeg er detaljer</div>
+        </div>
+        <div className='col-xs-12 tags'>
+          {this.props.belt.tags && this.props.belt.tags.map((tag, idx) => {
+            const btnClass = tag.selected ? 'btn-success' : 'btn-default';
+            return <span className={`btn ${btnClass}`} key={idx} onClick={() => {
+              this.props.onTagClick(idx);
+            }}>{tag.name}</span>;
+          })}
+        </div>
+        {this.props.custom}
+        <div className='belt-wrapper'>
+          {this.props.belt.works && (
+            <div className='button-wrapper col-xs-12 noselect'>
+              <div className='left-btn scroll-btn text-center' onClick={this.props.onScrollLeft}>{'<'}</div>
+              <div className='right-btn scroll-btn text-center' onClick={this.props.onScrollRight}>{'>'}</div>
+            </div>
+          )}
+          <div className='works-wrapper col-xs-12 noselect'>
+            <div className='works' style={{transform: `translate3d(${scrollPos}, 0px, 0px)`, filter: this.state.showDetails ? 'sepia(100%)' : null}}>
+              {this.props.belt.works && this.props.belt.works.map((work, idx) => {
+                return (
+                  <div className='work' id={`work-${idx}`} key={idx}>
+                    <div className='cover-image-wrapper'>
+                      <img alt="" className='cover-image' src={work.cover}/>
+                    </div>
+                    <div className='metakompas-description'>
+                      {work.metakompasDescription}
+                    </div>
                   </div>
-                  <div className='metakompas-description'>
-                    {work.metakompasDescription}
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
