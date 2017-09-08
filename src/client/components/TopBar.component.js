48f2b29fc3b7ae4ab2d86f8b01e83651217cd24a
Kasper Fuglsang <kfu@dbc.dk>
2017-09-08 11:42:20 +0200

    #4 added logo and home link

--- src/client/components/TopBar.component.js
+++ src/client/components/TopBar.component.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {HISTORY_PUSH} from '../redux/middleware';
 
 export default function TopBar(props) { // eslint-disable-line no-unused-vars
   return (
     <div className='row topbar'>
-      <div className='col-xs-6 text-left header'>
-        <h1>Læsekompasset</h1>
+      <div className='col-xs-6 text-left header' onClick={() => {
+        props.dispatch({type: HISTORY_PUSH, path: '/'});
+      }}>
+        <div><img src='/logo.png' alt='logo'/></div>
+        <div><h1>Læsekompasset</h1></div>
       </div>
-      <div className='col-xs-6 text-right login'>Log ind</div>
+      <div className='col-xs-6 text-right login'><span>Log ind</span></div>
     </div>
   );
 }
