e25ec94ee447eef41bf54fd878ae0b1dfeda7cf8
Kasper Fuglsang <kfu@dbc.dk>
2017-09-19 12:20:40 +0200

    respect description \n. fix belt margin

--- src/client/components/filter/BootstrapDropdown.component.js
+++ src/client/components/filter/BootstrapDropdown.component.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Dropdown = (props) => {
   return (
-    <div className="dropdown">
-      <span className="dropdown-toggle" id={props.id} data-toggle="dropdown">{props.selected}</span>
-      <ul className="dropdown-menu" role="menu" aria-labelledby={props.id}>
+    <div className='dropdown'>
+      <span className='dropdown-toggle' id={props.id} data-toggle='dropdown'>{props.selected}</span>
+      <ul className='dropdown-menu' role='menu' aria-labelledby={props.id}>
         {props.options.map((option, idx) => <li role='presentation' key={idx}><a onClick={e => {
           props.onChange(option);
           e.preventDefault();
           return false;
-        }} role="menuitem" tabIndex="-1" href="#">{option}</a></li>)}
+        }} role='menuitem' tabIndex='-1' href=''>{option}</a></li>)}
       </ul>
     </div>
   );
