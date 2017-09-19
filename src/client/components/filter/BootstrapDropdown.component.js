baf9d58fee2ee755c79c7f521b7d19d0fbba0777
Kasper Fuglsang <kfu@dbc.dk>
2017-09-19 14:03:57 +0200

    eslint: split lines

--- src/client/components/filter/BootstrapDropdown.component.js
+++ src/client/components/filter/BootstrapDropdown.component.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Dropdown = (props) => {
   return (
     <div className='dropdown'>
-      <span className='dropdown-toggle' id={props.id} data-toggle='dropdown'>{props.selected}<span style={{borderBottom: 'none', margin: '0px 0.2em 0px 0.4em'}}className='caret'></span></span>
+      <span className='dropdown-toggle' id={props.id} data-toggle='dropdown'>
+        {props.selected}
+        <span style={{borderBottom: 'none', margin: '0px 0.2em 0px 0.4em'}}className='caret'></span>
+      </span>
       <ul className='dropdown-menu' role='menu' aria-labelledby={props.id}>
         {props.options.map((option, idx) => <li role='presentation' key={idx}><a onClick={e => {
           props.onChange(option);
