543eb68f35e61b7739fc5e9e0d2468cc8aa3fde8
Kasper Fuglsang <kfu@dbc.dk>
2017-11-08 10:30:55 +0100

    use exportTaxonomy.json (from metakompasset) to generate filters at filterpage, and to show labels at workpage

--- src/client/components/filter/EditFilters.component.js
+++ src/client/components/filter/EditFilters.component.js
@@ -17,24 +17,24 @@ const EditFilters = (props) => {
   return (
     <div className={props.edit ? 'edit-filters col-xs-12 text-left' : 'edit-filters col-xs-12 text-left hidden'}>
       {props.filters.map((f1, idx1) => {
-        if (f1.children) {
+        if (f1.items) {
           return (
             <div key={idx1} className='first-level col-xs-4'>
               <h3>{f1.title}</h3>
-              {f1.children.map((f2, idx2) => {
-                if (f2.children) {
+              {f1.items.map((f2, idx2) => {
+                if (f2.items) {
 
                   // find selected filters of this category
-                  const selected = intersection(props.selectedFilters.map(f => f.id), f2.children.map(f => f.id));
+                  const selected = intersection(props.selectedFilters.map(f => f.id), f2.items.map(f => f.id));
                   const selectedClass = selected.length > 0 ? ' contains-selected' : '';
 
                   return (
                     <div key={idx2} className={`second-level col-xs-12${selectedClass}`}>
                       <h4 onClick={() => {
                         props.onExpandFiltersToggle(f2.id);
-                      }}>{f2.title}<span>{f2.children.length}</span></h4>
-                      {props.expandedFilters.indexOf(f2.id) >= 0 && f2.children.map((f3, idx3) => {
-                        if (!f3.children) {
+                      }}>{f2.title}<span>{f2.items.length}</span></h4>
+                      {props.expandedFilters.indexOf(f2.id) >= 0 && f2.items.map((f3, idx3) => {
+                        if (!f3.items) {
                           return <FilterButton key={idx3} filter={f3} selected={props.selectedFilters.map(f => f.id).indexOf(f3.id) >= 0} onFilterToggle={props.onFilterToggle}/>;
                         }
                         return null;
