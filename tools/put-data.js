81dc16d5930db88eec9c8fac581e74cbe9e13548
Kasper Fuglsang <kfu@dbc.dk>
2017-11-13 13:56:01 +0100

    inject-metakompas script will upload cover images located in projectpath/covers folder

--- tools/put-data.js
+++ tools/put-data.js
@@ -2,12 +2,14 @@
 'use strict';
 const request = require('superagent');
 const {uniqBy} = require('lodash');
+const fs = require('fs');
 
 const HOST = process.argv[2];
 const taxonomy = require('../src/client/data/exportTaxonomy.json');
 const pidinfo = require('../src/client/data/pidinfo.json');
 const tags = require('../src/client/data/exportTags.json');
 const librarianRecommends = require('../src/client/data/librarian-recommends.json');
+const COVER_FOLDER = process.cwd() + '/covers/';
 
 async function doWork() {
   await request.put(`${HOST}/v1/taxonomy`)
@@ -30,6 +32,22 @@ async function doWork() {
     .set('Content-Type', 'application/json')
     .send(uniqueTags);
   console.log('Uploaded tags'); // eslint-disable-line
+
+  var covers = fs.readdirSync(COVER_FOLDER);
+  if (covers) {
+    for (let i=0; i < covers.length; i++) {
+      const fileName = covers[i];
+      if (fileName.toLowerCase().indexOf('.jpg') === -1) {
+        continue;
+      }
+      const cover = fs.readFileSync(COVER_FOLDER + fileName);
+      const pid = fileName.replace(/\.jpg/ig, '');
+      await request.put(`${HOST}/v1/image/${pid}`)
+        .set('Content-Type', 'image/jpeg')
+        .send(cover);
+      console.log(`Uploaded cover for pid ${pid}`); // eslint-disable-line
+    }
+  }
 }
 
 doWork()
