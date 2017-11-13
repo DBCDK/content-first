ba7f6a2f79e5c2160c40eeecddde8fd02a8c75bc
Soeren Vibjerg <vibjerg@gmail.com>
2017-11-13 13:35:53 +0100

    Add mood profile belt

---
+++ src/client/components/profile/ProfileMoodBelt.component.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {ON_ADD_PROFILE_TAG, ON_REMOVE_PROFILE_TAG} from '../../redux/profile.reducer';
+import '../../style/components/profileBelt.css';
+import Checkmark from '../svg/Checkmark';
+
+const Tag = ({tag, isSelected, dispatch})=> (
+  <div key={tag.label} className={`tag ${isSelected ? 'is-selected' : ''}`} onClick={() => dispatch({type: isSelected ? ON_REMOVE_PROFILE_TAG : ON_ADD_PROFILE_TAG, tag})}>
+    <div className="tag-background">
+      <img src={`https://www.lorempixel.com/300/300?cache=${tag.label}`} alt={tag.label} />
+    </div>
+    <span className="tag-checked"><Checkmark /></span>
+    <span className="tag-label raleway">{tag.label}</span>
+  </div>
+);
+
+class ProfileMoodBelt extends React.Component {
+  isTagSelected(tag) {
+    return this.props.profileState.tags.filter(({label}) => label === tag.label).length === 1;
+  }
+
+  render() {
+    return (
+      <div className="profile-belt">
+        {this.props.moods.map(tag => <Tag key={tag.label} tag={tag} dispatch={this.props.dispatch} isSelected={this.isTagSelected(tag)} />)}
+      </div>
+    );
+  }
+}
+export default connect(
+  // Map redux state to props
+  (state) => {
+    return {profileState: state.profileReducer};
+  }
+)(ProfileMoodBelt);
