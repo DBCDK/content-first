28e658d9975d78dd10c516f64a8c7ccbfbcbb025
Soeren Vibjerg <vibjerg@gmail.com>
2017-11-28 13:56:08 +0100

    fix problem when initiating a new profile

--- src/client/redux/profile.reducer.js
+++ src/client/redux/profile.reducer.js
@@ -1,4 +1,7 @@
-import {addElementsToProfiles, removeElementFromProfiles} from '../utils/profile';
+import {
+  addElementsToProfiles,
+  removeElementFromProfiles
+} from '../utils/profile';
 
 const defaultProfile = {
   moods: [],
@@ -33,22 +36,52 @@ const defaultState = {
       {label: 'konventionel', image: '/moods/konventionel.jpg'}
     ],
     genres: [
-      {label: 'Biografiske romaner', image: '/genres/small/biografiske-romaner.jpg'},
-      {label: 'Biografiske skuespil', image: '/genres/small/biografiske-skuespil.jpg'},
+      {
+        label: 'Biografiske romaner',
+        image: '/genres/small/biografiske-romaner.jpg'
+      },
+      {
+        label: 'Biografiske skuespil',
+        image: '/genres/small/biografiske-skuespil.jpg'
+      },
       {label: 'Brevromaner', image: '/genres/small/brevromaner.jpg'},
-      {label: 'Eventyr, sagn og myter', image: '/genres/small/eventyr-sagn-og-myter.jpg'},
-      {label: 'Eventyrlige fortællinger', image: '/genres/small/eventyrlige-fortaellinger.jpg'},
-      {label: 'Fantasy, fremtid og science fiction', image: '/genres/small/fantasy-fremtid-og-science-fiction.jpg'},
+      {
+        label: 'Eventyr, sagn og myter',
+        image: '/genres/small/eventyr-sagn-og-myter.jpg'
+      },
+      {
+        label: 'Eventyrlige fortællinger',
+        image: '/genres/small/eventyrlige-fortaellinger.jpg'
+      },
+      {
+        label: 'Fantasy, fremtid og science fiction',
+        image: '/genres/small/fantasy-fremtid-og-science-fiction.jpg'
+      },
       {label: 'Humor og satire', image: '/genres/small/humor-og-satire.jpg'},
-      {label: 'I Danmark', image: '/genres/small/i-danmark---i-andre-lande.jpg'},
-      {label: 'Krimi, gys og spænding', image: '/genres/small/krimi-gys-og-spaending.jpg'},
-      {label: 'Let at læse', image: '/genres/small/let-at-laese---for-voksne.jpg'},
+      {
+        label: 'I Danmark',
+        image: '/genres/small/i-danmark---i-andre-lande.jpg'
+      },
+      {
+        label: 'Krimi, gys og spænding',
+        image: '/genres/small/krimi-gys-og-spaending.jpg'
+      },
+      {
+        label: 'Let at læse',
+        image: '/genres/small/let-at-laese---for-voksne.jpg'
+      },
       {label: 'Noveller', image: '/genres/small/noveller.jpg'},
       {label: 'Poesi', image: '/genres/small/poesi.jpg'},
       {label: 'Robinsonader', image: '/genres/small/robinsonader.jpg'},
-      {label: 'Samfund, arbejde og dagligliv', image: '/genres/small/samfund-arbejde-og-dagligliv.jpg'},
+      {
+        label: 'Samfund, arbejde og dagligliv',
+        image: '/genres/small/samfund-arbejde-og-dagligliv.jpg'
+      },
       {label: 'Tegneserier', image: '/genres/small/tegneserier.jpg'},
-      {label: 'Tilbage i historien', image: '/genres/small/tilbage-i-historien.jpg'},
+      {
+        label: 'Tilbage i historien',
+        image: '/genres/small/tilbage-i-historien.jpg'
+      },
       {label: 'Westerns', image: '/genres/small/westerns.jpg'}
     ],
     archetypes: [
@@ -56,21 +89,33 @@ const defaultState = {
         label: 'romantikeren',
         image: '/archetypes/romantikeren.jpg',
         moods: ['fantasifuld', 'erotisk'],
-        likes: ['870970-basis:52038014', '870970-basis:52530423', '870970-basis:52939321'],
+        likes: [
+          '870970-basis:52038014',
+          '870970-basis:52530423',
+          '870970-basis:52939321'
+        ],
         authors: ['Carsten Jensen', 'Hanne Vibeke Holst']
       },
       {
         label: 'hestepigen',
         image: '/archetypes/hestepigen.jpg',
         moods: ['fantasifuld', 'dramatisk'],
-        likes: ['870970-basis:52038014', '870970-basis:52530423', '870970-basis:52939321'],
+        likes: [
+          '870970-basis:52038014',
+          '870970-basis:52530423',
+          '870970-basis:52939321'
+        ],
         authors: ['Anne Lise Marstrand Jørgensen']
       },
       {
         label: 'kynikeren',
         image: '/archetypes/kynikeren.jpg',
         moods: ['intellektuel', 'frygtelig', 'kompleks'],
-        likes: ['870970-basis:52038014', '870970-basis:52530423', '870970-basis:52939321'],
+        likes: [
+          '870970-basis:52038014',
+          '870970-basis:52530423',
+          '870970-basis:52939321'
+        ],
         authors: ['Kim Leine', 'Paula Hawkins']
       }
     ],
@@ -79,35 +124,55 @@ const defaultState = {
         label: 'Anne Lise Marstrand Jørgensen',
         image: '/authors/anne-lise-marstrand-joergensen.jpg',
         genres: ['fantasifuld', 'erotisk'],
-        likes: ['870970-basis:52038014', '870970-basis:52530423', '870970-basis:52939321'],
+        likes: [
+          '870970-basis:52038014',
+          '870970-basis:52530423',
+          '870970-basis:52939321'
+        ],
         byline: 'about the author'
       },
       {
         label: 'Carsten Jensen',
         image: '/authors/carsten-jensen.jpg',
         genres: ['fantasifuld', 'dramatisk'],
-        likes: ['870970-basis:52038014', '870970-basis:52530423', '870970-basis:52939321'],
+        likes: [
+          '870970-basis:52038014',
+          '870970-basis:52530423',
+          '870970-basis:52939321'
+        ],
         byline: 'about the author'
       },
       {
         label: 'Hanne Vibeke Holst',
         image: '/authors/hanne-vibeke-holst.jpg',
         genres: ['intellektuel', 'frygtelig', 'kompleks'],
-        likes: ['870970-basis:52038014', '870970-basis:52530423', '870970-basis:52939321'],
+        likes: [
+          '870970-basis:52038014',
+          '870970-basis:52530423',
+          '870970-basis:52939321'
+        ],
         byline: 'about the author'
       },
       {
         label: 'Kim Leine',
         image: '/authors/kim-leine.jpg',
         genres: ['intellektuel', 'frygtelig', 'kompleks'],
-        likes: ['870970-basis:52038014', '870970-basis:52530423', '870970-basis:52939321'],
+        likes: [
+          '870970-basis:52038014',
+          '870970-basis:52530423',
+          '870970-basis:52939321'
+        ],
         byline: 'about the author'
       },
       {
         label: 'Paula Hawkins',
         image: '/authors/paula-hawkins.jpg',
         genres: ['intellektuel', 'frygtelig', 'kompleks'],
-        likes: ['870970-basis:52038014', '870970-basis:52530423', '870970-basis:52939321'],
+        likes: [
+          '870970-basis:52038014',
+          '870970-basis:52530423',
+          '870970-basis:52939321'
+        ],
         byline: 'about the author'
       }
     ]
@@ -117,8 +182,10 @@ const defaultState = {
 export const ON_ADD_PROFILE_ELEMENT = 'ON_ADD_PROFILE_ELEMENT';
 export const ON_REMOVE_PROFILE_ELEMENT = 'ON_REMOVE_PROFILE_ELEMENT';
 export const ON_ADD_PROFILE_ARCHETYPE = 'ON_ADD_PROFILE_ARCHETYPE';
-export const ON_PROFILE_RECOMMENDATIONS_REQUEST = 'ON_PROFILE_RECOMMENDATIONS_REQUEST';
-export const ON_PROFILE_RECOMMENDATIONS_RESPONSE = 'ON_PROFILE_RECOMMENDATIONS_RESPONSE';
+export const ON_PROFILE_RECOMMENDATIONS_REQUEST =
+  'ON_PROFILE_RECOMMENDATIONS_REQUEST';
+export const ON_PROFILE_RECOMMENDATIONS_RESPONSE =
+  'ON_PROFILE_RECOMMENDATIONS_RESPONSE';
 export const ON_USER_DETAILS_REQUEST = 'ON_USER_DETAILS_REQUEST';
 export const ON_USER_DETAILS_RESPONSE = 'ON_USER_DETAILS_RESPONSE';
 export const ON_LOGOUT_REQUEST = 'ON_LOGOUT_REQUEST';
@@ -126,16 +193,32 @@ export const ON_LOGOUT_RESPONSE = 'ON_LOGOUT_RESPONSE';
 export const ON_PROFILE_CREATE_TASTE = 'ON_PROFILE_CREATE_TASTE';
 export const ON_PROFILE_SELECT_TASTE = 'ON_PROFILE_SELECT_TASTE';
 export const ON_PROFILE_LOAD_PROFILES = 'ON_PROFILE_LOAD_PROFILES';
-export const ON_PROFILE_LOAD_PROFILES_RESPONSE = 'ON_PROFILE_LOAD_PROFILES_RESPONSE';
-export const ON_PROFILE_REMOVE_CURRENT_PROFILE = 'ON_PROFILE_REMOVE_CURRENT_PROFILE';
+export const ON_PROFILE_LOAD_PROFILES_RESPONSE =
+  'ON_PROFILE_LOAD_PROFILES_RESPONSE';
+export const ON_PROFILE_REMOVE_CURRENT_PROFILE =
+  'ON_PROFILE_REMOVE_CURRENT_PROFILE';
 
 const profileReducer = (state = defaultState, action) => {
   switch (action.type) {
     case ON_ADD_PROFILE_ELEMENT: {
-      return Object.assign({}, state, addElementsToProfiles(state.profileTastes, {[action.elementType]: [action.element.label]}));
+      return Object.assign(
+        {},
+        state,
+        addElementsToProfiles(state.profileTastes, {
+          [action.elementType]: [action.element.label]
+        })
+      );
     }
     case ON_REMOVE_PROFILE_ELEMENT: {
-      return Object.assign({}, state, removeElementFromProfiles(state.profileTastes, action.elementType, action.element.label));
+      return Object.assign(
+        {},
+        state,
+        removeElementFromProfiles(
+          state.profileTastes,
+          action.elementType,
+          action.element.label
+        )
+      );
     }
     case ON_ADD_PROFILE_ARCHETYPE: {
       const updateElements = {
@@ -143,40 +226,82 @@ const profileReducer = (state = defaultState, action) => {
         moods: action.element.moods,
         authors: action.element.authors
       };
-      return Object.assign({}, state, addElementsToProfiles(state.profileTastes, updateElements));
+      return Object.assign(
+        {},
+        state,
+        addElementsToProfiles(state.profileTastes, updateElements)
+      );
     }
     case ON_PROFILE_RECOMMENDATIONS_REQUEST:
-      return Object.assign({}, state, {recommendations: Object.assign({}, state.recommendations, {isLoading: true})});
+      return Object.assign({}, state, {
+        recommendations: Object.assign({}, state.recommendations, {
+          isLoading: true
+        })
+      });
     case ON_PROFILE_RECOMMENDATIONS_RESPONSE:
-      return Object.assign({}, state, {recommendations: {elements: action.recommendations, isLoading: false}});
+      return Object.assign({}, state, {
+        recommendations: {elements: action.recommendations, isLoading: false}
+      });
     case ON_USER_DETAILS_REQUEST:
-      return Object.assign({}, state, {user: Object.assign({}, state.user, {isLoading: true})});
+      return Object.assign({}, state, {
+        user: Object.assign({}, state.user, {isLoading: true})
+      });
     case ON_USER_DETAILS_RESPONSE:
       if (!action.user) {
         return Object.assign({}, state, {user: defaultState.user});
       }
-      return Object.assign({}, state, {user: Object.assign({}, action.user, {isLoggedIn: true, isLoading: false})});
+      return Object.assign({}, state, {
+        user: Object.assign({}, action.user, {
+          isLoggedIn: true,
+          isLoading: false
+        })
+      });
     case ON_LOGOUT_REQUEST:
-      return Object.assign({}, state, {user: Object.assign({}, state.user, {isLoading: true})});
+      return Object.assign({}, state, {
+        user: Object.assign({}, state.user, {isLoading: true})
+      });
     case ON_LOGOUT_RESPONSE:
-      return Object.assign({}, state, {user: Object.assign({}, state.user, {isLoggedIn: false, isLoading: false})});
+      return Object.assign({}, state, {
+        user: Object.assign({}, state.user, {
+          isLoggedIn: false,
+          isLoading: false
+        })
+      });
     case ON_PROFILE_CREATE_TASTE: {
       const currentTaste = action.name;
-      const profiles = Object.assign({}, state.profiletastes.profiles, {[currentTaste]: Object.assign({}, defaultProfile)});
-      return Object.assign({}, state, {profileTastes: {profiles, currentTaste}});
+      const profiles = Object.assign({}, state.profiletastes.profiles, {
+        [currentTaste]: Object.assign({}, defaultProfile)
+      });
+      return Object.assign({}, state, {
+        profileTastes: {profiles, currentTaste}
+      });
     }
     case ON_PROFILE_SELECT_TASTE: {
       const currentTaste = action.name;
-      return Object.assign({}, state, {profileTastes: Object.assign({}, state.profileTastes, {currentTaste})});
+      return Object.assign({}, state, {
+        profileTastes: Object.assign({}, state.profileTastes, {currentTaste})
+      });
     }
     case ON_PROFILE_LOAD_PROFILES_RESPONSE: {
-      return Object.assign({}, state, {profileTastes: Object.assign({}, action.profileTastes, {loading: false})});
+      return Object.assign({}, state, {
+        profileTastes: Object.assign(
+          {},
+          action.profileTastes || state.profileTastes,
+          {loading: false}
+        )
+      });
     }
     case ON_PROFILE_LOAD_PROFILES: {
-      return Object.assign({}, state, {profileTastes: Object.assign({}, state.profileTastes, {loading: true})});
+      return Object.assign({}, state, {
+        profileTastes: Object.assign({}, state.profileTastes, {loading: true})
+      });
     }
     case ON_PROFILE_REMOVE_CURRENT_PROFILE: {
-      return Object.assign({}, state, {profileTastes: Object.assign({}, state.profileTastes, {currentTaste: null})});
+      return Object.assign({}, state, {
+        profileTastes: Object.assign({}, state.profileTastes, {
+          currentTaste: null
+        })
+      });
     }
     default:
       return state;
--- src/client/utils/profile.js
+++ src/client/utils/profile.js
@@ -2,16 +2,19 @@ import unique from './unique';
 
 export function saveProfiles(profiles, currentTaste) {
   if (window && window.localStorage) {
-    localStorage.setItem('contentFirstProfile', JSON.stringify({profiles, currentTaste}));
+    localStorage.setItem(
+      'contentFirstProfile',
+      JSON.stringify({profiles, currentTaste})
+    );
   }
 }
 
 export function getProfiles(cb) {
   setTimeout(() => {
     if (window && window.localStorage) {
-      return cb(JSON.parse(localStorage.getItem('contentFirstProfile') || {}));
+      return cb(JSON.parse(localStorage.getItem('contentFirstProfile')));
     }
-    return cb({});
+    return cb();
   }, 500);
 }
 
@@ -21,20 +24,36 @@ export function getAllSelectedTags(profile) {
 
 export function addElementsToProfiles({profiles, currentTaste}, obj) {
   const profile = Object.keys(obj).reduce((currentProfile, beltName) => {
-    currentProfile[beltName] = unique([...currentProfile[beltName], ...obj[beltName]]);
+    currentProfile[beltName] = unique([
+      ...currentProfile[beltName],
+      ...obj[beltName]
+    ]);
     if (beltName !== 'archetypes') {
-      currentProfile.allSelectedTags = unique([...currentProfile.allSelectedTags, ...obj[beltName]]);
+      currentProfile.allSelectedTags = unique([
+        ...currentProfile.allSelectedTags,
+        ...obj[beltName]
+      ]);
     }
     return currentProfile;
   }, profiles[currentTaste]);
-  const updatedProfiles = Object.assign({}, profiles, {[currentTaste]: profile});
+  const updatedProfiles = Object.assign({}, profiles, {
+    [currentTaste]: profile
+  });
   return {profiles: updatedProfiles, profile};
 }
 
-export function removeElementFromProfiles({profiles, currentTaste}, beltname, value) {
+export function removeElementFromProfiles(
+  {profiles, currentTaste},
+  beltname,
+  value
+) {
   const profile = profiles[currentTaste];
   profile[beltname] = profile[beltname].filter(element => element !== value);
-  profile.allSelectedTags = profile.allSelectedTags.filter(element => element !== value);
-  const updatedProfiles = Object.assign({}, profiles, {[currentTaste]: profile});
+  profile.allSelectedTags = profile.allSelectedTags.filter(
+    element => element !== value
+  );
+  const updatedProfiles = Object.assign({}, profiles, {
+    [currentTaste]: profile
+  });
   return {profiles: updatedProfiles, profile};
 }
