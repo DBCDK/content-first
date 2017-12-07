e240d56440ae13248120c667bac272ddb444f221
Kasper Fuglsang <kfu@dbc.dk>
2017-12-07 13:12:36 +0100

    added checkmark menu used for adding books to lists

--- src/client/components/work/WorkItem.component.js
+++ src/client/components/work/WorkItem.component.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import Image from '../Image.component';
-import CheckmarkButton from '../general/CheckmarkButton.component';
+import CheckmarkMenu, {MenuItem} from '../general/CheckmarkMenu.component';
 
 class WorkItem extends React.Component {
   render() {
+    const systemLists = [
+      {
+        id: 'blah',
+        name: 'Har læst',
+        elements: ['870970-basis:51772679', 'somepid2']
+      },
+      {
+        id: 'blah2',
+        name: 'Vil læse',
+        elements: ['somepid1', 'somepid2']
+      }
+    ];
+
     const tax_description = this.props.work.book.taxonomy_description || this.props.work.book.description;
     return (
       <div className='work' id={`work-${this.props.id}`}>
-        <div className='cover-image-wrapper' onClick={() => {
-          this.props.onCoverClick(this.props.work.book.pid);
-        }}>
-          <div className='cover-image'>
+        <div className='cover-image-wrapper'>
+          <div className='cover-image' onClick={() => {
+            this.props.onCoverClick(this.props.work.book.pid);
+          }}>
             <Image urls={[
               `https://metakompasset.demo.dbc.dk/api/cover/${encodeURIComponent(this.props.work.book.pid)}`,
               `/v1/image/${encodeURIComponent(this.props.work.book.pid)}`,
               '/default-book-cover.png'
             ]}/>
           </div>
-          <CheckmarkButton
-            label="Husk"
-            marked={this.props.marked}
-            onClick={(e) => {
-              e.stopPropagation();
-              this.props.onRememberClick(this.props.work);
-            }}/>
+          <CheckmarkMenu
+            text="Husk"
+            marked={true}
+            onClick={() => (console.log('header click'))}>
+            {systemLists.map(l => (
+              <MenuItem
+                key={l.id}
+                text={l.name}
+                marked={l.elements.indexOf(this.props.work.book.pid) !== -1}
+                onClick={() => (console.log(this.props.work.book, l))}/>
+            ))}
+            <MenuItem
+              key="addToList"
+              text="Tilføj til liste"
+              onClick={() => (console.log(this.props.work.book, 'tilføj'))}/>
+          </CheckmarkMenu>
         </div>
         <div className='metakompas-description'>
           {tax_description && tax_description.split('\n').map((line, idx) => <p key={idx}>{line}</p>)}
