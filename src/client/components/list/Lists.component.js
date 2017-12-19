0f86e1c558630d755ffa5e5c1f654feca15531d7
Kasper Fuglsang <kfu@dbc.dk>
2017-12-19 08:59:43 +0100

    use history push for faster navigation

--- src/client/components/list/Lists.component.js
+++ src/client/components/list/Lists.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {LIST_LOAD_REQUEST, SYSTEM_LIST} from '../../redux/list.reducer';
+import {HISTORY_PUSH} from '../../redux/middleware';
 
 class Lists extends React.Component {
   componentDidMount() {
@@ -12,13 +13,25 @@ class Lists extends React.Component {
       <div className="lists-page">
         <h1>Mine Lister</h1>
         {lists.filter(list => list.type !== SYSTEM_LIST).map(list => (
-          <a href={`/lister/${list.id}`} className="list">
+          <a href={`/lister/${list.id}`}
+            className="list"
+            onClick={e => {
+              this.props.dispatch({type: HISTORY_PUSH, path: `/lister/${list.id}`});
+              e.preventDefault();
+            }}
+          >
             <h2>{list.title}</h2>
             <p>{list.description}</p>
           </a>
         ))}
         <div>
-          <a href="/lister/opret" className="btn btn-primary">
+          <a href="/lister/opret"
+            className="btn btn-primary"
+            onClick={e => {
+              this.props.dispatch({type: HISTORY_PUSH, path: '/lister/opret'});
+              e.preventDefault();
+            }}
+          >
             Opret ny liste
           </a>
         </div>
