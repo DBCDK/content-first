bb90502869c8bcca6ac0a74d7e7a9a8f0164bace
Kasper Fuglsang <kfu@dbc.dk>
2018-01-05 11:12:49 +0100

    mock json data in jests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,20 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({adapter: new Adapter()});
 require('jest-localstorage-mock');
+
+const mockedData = [
+  'exportTaxonomy.json',
+  'ranked-profiles.json',
+  'similar-pids.json',
+  'exportTags.json',
+  'librarian-recommends.json',
+  'pidinfo.json'
+].forEach(mockJson => {
+  jest.mock(
+    `./data/${mockJson}`,
+    () => require(`./data/examples/${mockJson}`),
+    {
+      virtual: true
+    }
+  );
+});
