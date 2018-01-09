b2a54d022d312425be663713b5b48cc64be44e5e
Kasper Fuglsang <kfu@dbc.dk>
2018-01-09 09:54:17 +0100

    fix lint

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,7 @@
 'use strict';
 // TODO: Remove this `raf` polyfill once the below issue is sorted
 // https://github.com/facebookincubator/create-react-app/issues/3199#issuecomment-332842582
-import raf from './raf-shim';
-
+import raf from './raf-shim'; // eslint-disable-line
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({adapter: new Adapter()});
