e1c4dea571734b4b1e36a0b2437c2e34c623dacc
Kasper Fuglsang <kfu@dbc.dk>
2018-01-09 09:48:33 +0100

    add rfa shim to remove jest warning

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,10 @@
 'use strict';
+// TODO: Remove this `raf` polyfill once the below issue is sorted
+// https://github.com/facebookincubator/create-react-app/issues/3199#issuecomment-332842582
+import raf from './raf-shim';
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({adapter: new Adapter()});
 require('jest-localstorage-mock');
+jest.mock('react-textarea-autosize', () => 'textarea');
