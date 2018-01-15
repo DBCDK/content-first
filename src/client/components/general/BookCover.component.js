2584586986bbc511009f5e6e0e691135ec74050f
Kasper Fuglsang <kfu@dbc.dk>
2018-01-15 11:13:01 +0100

    fix cover not changing at work page

--- src/client/components/general/BookCover.component.js
+++ src/client/components/general/BookCover.component.js
@@ -4,24 +4,21 @@ class BookCover extends React.Component {
   constructor(props) {
     super(props);
     this.state = {current: 0};
-    this.urls = [
-      `https://metakompasset.demo.dbc.dk/api/cover/${encodeURIComponent(
-        props.book.pid
-      )}`,
-      `/v1/image/${encodeURIComponent(props.book.pid)}`,
+  }
+  render() {
+    const urls = [
+      `https://metakompasset.demo.dbc.dk/api/cover/${encodeURIComponent(this.props.book.pid)}`,
+      `/v1/image/${encodeURIComponent(this.props.book.pid)}`,
       '/default-book-cover.png'
     ];
-  }
-
-  render() {
     return (
       <img
         alt={this.props.book.title || ''}
         className={'high-contrast ' + (this.props.className || '')}
-        src={this.urls[this.state.current]}
+        src={urls[this.state.current]}
         onError={() => {
           this.setState({
-            current: Math.min(this.state.current + 1, this.urls.length - 1)
+            current: Math.min(this.state.current + 1, urls.length - 1)
           });
         }}
       />
