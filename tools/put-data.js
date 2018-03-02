6079d48737d1ffa8d82391b1b39c686f93f7b3d8
kfudbc <kfudbc@users.noreply.github.com>
2018-03-02 14:43:08 +0100

    download json files, when injecting (#255)
    * download json files, when injecting
    
    * moved fetching metakompas-data to its own script

--- tools/put-data.js
+++ tools/put-data.js
@@ -3,23 +3,49 @@
 const request = require('superagent');
 const {uniqBy} = require('lodash');
 const fs = require('fs');
+const targz = require('targz');
+const config = require('server/config');
 
-const HOST = process.argv[2];
-const taxonomy = require('../src/data/exportTaxonomy.json');
-const pidinfo = require('../src/data/pidinfo.json');
-const tags = require('../src/data/exportTags.json');
-const librarianRecommends = require('../src/data/librarian-recommends.json');
-const COVER_FOLDER = process.cwd() + '/src/data/';
+const JSON_FILES_URL = process.env.JSON_FILES_URL;
+const INTERNAL_PORT = config.server.internalPort;
+const PORT = config.server.port;
+const HOST = 'http://localhost:' + INTERNAL_PORT;
+const HOWRU = 'http://localhost:' + PORT + '/howru';
+const DATA_DIR = process.cwd() + '/src/data/';
 
+const waitForReady = async () => {
+  let ready = false;
+  while (!ready) {
+    try {
+      await request.get(`${HOWRU}`);
+      ready = true;
+    } catch (e) {
+      await new Promise(resolve => {
+        setTimeout(() => resolve(), 500);
+      });
+      console.log('waiting for ' + HOWRU);
+    }
+  }
+};
 async function doWork() {
-  // await request.put(`${HOST}/v1/taxonomy`)
-  //   .set('Content-Type', 'application/json')
-  //   .send(taxonomy);
-  // console.log('Uploaded taxonomy'); // eslint-disable-line
+  const taxonomy = require('../src/data/exportTaxonomy.json');
+  const pidinfo = require('../src/data/pidinfo.json');
+  const tags = require('../src/data/exportTags.json');
+  const librarianRecommends = require('../src/data/librarian-recommends.json');
+
+  await waitForReady();
   await request
     .put(`${HOST}/v1/books`)
     .set('Content-Type', 'application/json')
-    .send(pidinfo);
+    .send(
+      pidinfo.map(entry =>
+        Object.assign(entry, {
+          unitId: entry.unitId || '',
+          bibliographicRecordId: entry.bibliographicRecordId || '-1'
+        })
+      )
+    );
+
   console.log('Uploaded books'); // eslint-disable-line
 
   const uniqueTags = uniqBy(tags, 'pid');
@@ -34,16 +60,17 @@ async function doWork() {
     .put(`${HOST}/v1/tags`)
     .set('Content-Type', 'application/json')
     .send(uniqueTags);
+
   console.log('Uploaded tags'); // eslint-disable-line
 
-  var covers = fs.readdirSync(COVER_FOLDER);
+  var covers = fs.readdirSync(DATA_DIR);
   if (covers) {
     for (let i = 0; i < covers.length; i++) {
       const fileName = covers[i];
       if (fileName.toLowerCase().indexOf('.jpg') === -1) {
         continue;
       }
-      const cover = fs.readFileSync(COVER_FOLDER + fileName);
+      const cover = fs.readFileSync(DATA_DIR + fileName);
       const pid = fileName.replace(/\.jpg/gi, '');
       await request
         .put(`${HOST}/v1/image/${pid}`)
