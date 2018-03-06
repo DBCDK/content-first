8d2ad47a262d7e33f03b8a4905c4e70d3b60ba99
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-03-06 14:14:59 +0100

    listelements as object references in backend #217

--- src/client/utils/requestLists.js
+++ src/client/utils/requestLists.js
@@ -11,6 +11,7 @@ import {SYSTEM_LIST} from '../redux/list.reducer';
 export const saveList = async list => {
   list = Object.assign({}, list);
   list._type = 'list';
+  list.list = list.list || [];
 
   // old-endpoint compat, refactor to just _id later
   list._public = list.public;
@@ -18,15 +19,39 @@ export const saveList = async list => {
     list._id = list._id || list.id;
   }
 
+  if (!list._id && list.list.length > 0) {
+    Object.assign(list, (await request.post('/v1/object').send({})).body.data);
+    list.id = list._key || list._id;
+  }
+
+  list.list = await Promise.all(
+    list.list.map(async o => {
+      const {book} = o;
+      try {
+        const saved = Object.assign({}, o, {
+          _type: 'list-entry',
+          book: null,
+          pid: book.pid,
+          _key: list.id,
+          _rev: null,
+          _public: list._public
+        });
+        return Object.assign(
+          saved,
+          (await request.post('/v1/object').send(saved)).body.data,
+          {book}
+        );
+      } catch (e) {
+        // possibly permission denied if not owner of list element
+        return o;
+      }
+    })
+  );
+
   const result = await request.post('/v1/object').send(
     Object.assign({}, list, {
       _rev: null,
-      list:
-        list.list &&
-        list.list.map(o => ({
-          pid: o.book.pid,
-          description: o.description || ''
-        }))
+      list: list.list && list.list.map(({_id}) => ({_id}))
     })
   );
   Object.assign(list, result.body.data);
@@ -68,9 +93,40 @@ export const loadRecentPublic = async () => {
 async function enrichList(list) {
   list.id = list.id || list._id;
   list.owner = list._owner;
-  list.list = list.list || [];
-  const pids = list.list.map(o => o.pid);
 
+  // Load list elements
+  list.list = list.list || [];
+  list.list = await Promise.all(
+    list.list.map(async o => {
+      try {
+        return o._id ? (await request.get('/v1/object/' + o._id)).body.data : o;
+      } catch (e) {
+        // might fail if permission denied, i.e. owner of list element changed permission.
+        return o;
+      }
+    })
+  );
+
+  // Add elements for open lists
+  if (list.open) {
+    let othersListEntries = (await request.get(
+      `/v1/object/find?type=list-entry&key=${list.id}&limit=100000`
+    )).body.data;
+
+    list.deleted = list.deleted || {};
+    othersListEntries = othersListEntries.filter(({_id}) => !list.deleted[_id]);
+
+    const listIds = list.list.map(o => o._id).filter(o => !!o);
+    othersListEntries = othersListEntries.filter(
+      ({_id}) => !listIds.includes(_id)
+    );
+
+    list.list = list.list.concat(othersListEntries);
+  }
+
+  // Load books into list
+  list.list = list.list.filter(o => o.pid);
+  const pids = list.list.map(o => o.pid);
   if (pids.length > 0) {
     const works = (await request.get('/v1/books/').query({pids})).body.data;
     const worksMap = works.reduce((map, w) => {
