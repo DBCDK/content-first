aff1083c77bf6abcd265e9b117ca79472c84aff5
Stanford Vinson <stvi@dbc.dk>
2018-03-14 11:26:49 +0100

    now prettier

--- src/client/components/list/ListCard.component.js
+++ src/client/components/list/ListCard.component.js
@@ -5,7 +5,6 @@ import ProfileImage from '../general/ProfileImage.component';
 import {Likes, Comments, Badge} from '../general/Icons';
 import Link from '../general/Link.component';
 
-
 class ListCard extends React.PureComponent {
   render() {
     const {list, style} = this.props;
@@ -13,55 +12,54 @@ class ListCard extends React.PureComponent {
     const elements = list.list;
     return (
       <div className="list-card" style={style}>
-      <Link href={`/lister/${list.id}`} >
-        <div className="list-card-covers">
-          <div className="list-card-covers-wrapper">
-            {elements.length > 0 &&
-              elements.slice(0, 3).map(e => {
-                return (
-                  <BookCover
-                    key={e.book.pid}
-                    className="list-card-cover"
-                    book={e.book}
-                  />
-                );
-              })}
+        <Link href={`/lister/${list.id}`}>
+          <div className="list-card-covers">
+            <div className="list-card-covers-wrapper">
+              {elements.length > 0 &&
+                elements.slice(0, 3).map(e => {
+                  return (
+                    <BookCover
+                      key={e.book.pid}
+                      className="list-card-cover"
+                      book={e.book}
+                    />
+                  );
+                })}
+            </div>
           </div>
-        </div>
-        <div className="list-card-summary">
-          <TruncateMarkup
-            lines={2}
-            ellipsis={
-              <span>
-                ...<Badge value={elements.length} className="ml1" />
-              </span>
-            }
-          >
-            <h3 className="list-card-title h-tight">
-              {list.title}
-              <Badge value={elements.length} className="ml1" />
-            </h3>
-          </TruncateMarkup>
-          <TruncateMarkup lines={3}>
-            <div className="list-card-description">{list.description}</div>
-          </TruncateMarkup>
-        </div>
-        <div className="list-card-bottom">
-          <div style={{display: 'inline-block'}}>
-            <ProfileImage
-              user={this.props.profile}
-              size={'35'}
-              namePosition={'right'}
-              type="list"
-              className="mb1"
-            />
+          <div className="list-card-summary">
+            <TruncateMarkup
+              lines={2}
+              ellipsis={
+                <span>
+                  ...<Badge value={elements.length} className="ml1" />
+                </span>
+              }
+            >
+              <h3 className="list-card-title h-tight">
+                {list.title}
+                <Badge value={elements.length} className="ml1" />
+              </h3>
+            </TruncateMarkup>
+            <TruncateMarkup lines={3}>
+              <div className="list-card-description">{list.description}</div>
+            </TruncateMarkup>
           </div>
-          <div style={{float: 'right', marginTop: 5}}>
-            <Likes value={14} />
-            <Comments value={14} className="ml1" />
+          <div className="list-card-bottom">
+            <div style={{display: 'inline-block'}}>
+              <ProfileImage
+                user={this.props.profile}
+                size={'35'}
+                namePosition={'right'}
+                type="list"
+                className="mb1"
+              />
+            </div>
+            <div style={{float: 'right', marginTop: 5}}>
+              <Likes value={14} />
+              <Comments value={14} className="ml1" />
+            </div>
           </div>
-        </div>
-
         </Link>
       </div>
     );
