1e2635541a3aa64fdaad16b52c171b0381cab678
Kasper Fuglsang <kfu@dbc.dk>
2018-03-22 12:08:33 +0100

    query param to support minus sign

--- src/client/redux/router.reducer.js
+++ src/client/redux/router.reducer.js
@@ -3,7 +3,7 @@ export const HISTORY_PUSH_FORCE_REFRESH = 'HISTORY_PUSH_FORCE_REFRESH';
 export const HISTORY_REPLACE = 'HISTORY_REPLACE';
 export const ON_LOCATION_CHANGE = 'ON_LOCATION_CHANGE';
 
-const PARAMS_REGEX = /[?&](\w*)=(\w*)/g;
+const PARAMS_REGEX = /[?&](\w*)=(.\w*)/g;
 const routerReducer = (state = {}, action) => {
   switch (action.type) {
     case ON_LOCATION_CHANGE: {
