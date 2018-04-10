1bc57542de10cc68f79eb67ebbcf0be30b9e1621
mtni <36037828+mtni@users.noreply.github.com>
2018-04-10 14:29:26 +0200

    317 share list on facebook (#388)
    * changed test link
    
    * hide share button
    
    * added server evaluated list on bot detection
    
    * enabled share button
    
    * SocialShareButton Done + ready for demo test
    
    * moved express-bot to external file + added extra crawler detecors - ready for another test
    
    * solved test errors
    
    * dobbel check
    
    * solved test errors
    
    * back to express-bot
    
    * descriptio fallback done
    
    * resolved test errors
    
    * solved comment corrections

--- src/server/external-meta.js
+++ src/server/external-meta.js
@@ -1,50 +1,85 @@
 'use strict';
 
 const express = require('express');
+const request = require('superagent');
 const router = express.Router({mergeParams: true});
 const asyncMiddleware = require('__/async-express').asyncMiddleware;
 const community = require('server/community');
+const config = require('./config');
+
+const showTitles = 3;
 
 router
   .route('/')
 
   .get(
     asyncMiddleware(async (req, res, next) => {
-
-      console.log('User Agent: ');
-      console.log(req.headers['user-agent']);
-
       if (JSON.stringify(res.locals) === JSON.stringify({})) {
-        console.log('... This is NOT a BOT - return normal page');
         return next();
       }
 
-      if (req.headers['user-agent'] === 'facebookexternalhit/1.1') {
-        console.log('THIS IS a Facebook bot!');
-      }
-
-      console.log('... This is a BOT! - return OG:META page');
-
       const listId = req.params.id;
       const list = await community.getObjectById(listId, {});
+      const host = req.get('host');
+
+      const listPids = await request
+        .get('http://' + host + '/v1/object/find')
+        .query({
+          type: 'list-entry',
+          key: listId
+        });
+
+      let aPids = listPids.body;
+      aPids = aPids.data;
+
+      const pids = aPids.map(p => p.pid);
+
+      const books = await request
+        .get('http://' + host + '/v1/books/')
+        .query({pids});
+
+      let aBooks = books.body;
+      aBooks = aBooks.data;
+
+      let titles = aBooks.map(b => b.book.title);
+      titles = titles.slice(0, showTitles).join(', ');
+
+      // Construct title shortner sentence according to showTitle number
+      const bookBooks = aBooks.length - showTitles > 1 ? 'bøger' : 'bog';
+
+      const andMore =
+        aBooks.length > showTitles
+          ? ' & ' + (aBooks.length - showTitles) + ' ' + bookBooks + ' mere'
+          : '';
+
+      titles += andMore;
+
+      // Evaluate meta content
+      const description =
+        list.data.description && list.data.description !== ''
+          ? list.data.description
+          : titles;
+      const img = list.data.image
+        ? list.data.image
+        : 'img/bookcase/NB-bogreol.jpg';
+
+      // Create Meta content in HTML markup
       const title = '<title>' + list.data.title + '</title>';
       const ogTitle =
         '<meta property="og:title" content="' + list.data.title + '" />';
       const ogDescription =
-        '<meta property="og:description" content="' +
-        list.data.description +
-        '" />';
-      const img = list.data.image
-        ? list.data.image
-        : 'img/bookcase/NB-bogreol.jpg';
+
+        '<meta property="og:description" content="' + description + '" />';
       const ogImage =
         '<meta property="og:image" content="/v1/image/' + img + '" />';
-
       const ogURL =
-        '<meta property="og:url" content="https://content-first.demo.dbc.dk/lister/' +
+        '<meta property="og:url" content="http://' +
+        config.server.hostname +
+        '/lister/' +
         listId +
         '" />';
 
+      // Build <head>
       const head =
         '<head>' +
         title +
@@ -54,9 +89,14 @@ router
         ogURL +
         '</head>';
 
+
+      // Build <body>
       const body = '<body>Hello Bot!</body>';
+
+      // Build <html>
       const html = '<html>' + head + body + '</html>';
 
+      // Return Page
       return res.send(html);
     })
   );
