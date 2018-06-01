ba6c7f6866fc1156374f42c3a0416def95526f51
stanvins <36958949+stanvins@users.noreply.github.com>
2018-06-01 09:58:51 +0200

    76 tracking user behavior stvi (#450)
    * adding an interaction reducer to track user behavior on bestil, husk, har læst, vil læse. These updates take effect on state and update in the database via the generic object endpoint
    
    * adding fetch dispatch from user.middleware, to instantiate the data from the db.
    
    * cleaned up comments and logging
    
    * removed space in comment
    
    * removed unnecessary state in reducer

---
+++ src/client/redux/interaction.middleware.js
@@ -0,0 +1,126 @@
+import {ON_SHORTLIST_TOGGLE_ELEMENT} from './shortlist.reducer';
+import {ON_LOCATION_CHANGE} from './router.reducer';
+import {LIST_TOGGLE_ELEMENT} from './list.reducer';
+import {ORDER} from './order.reducer';
+import request from 'superagent';
+import {fetchObjects} from '../utils/requester';
+import {
+  INTERACTION,
+  FETCH_INTERACTIONS,
+  FETCH_INTERACTIONS_ERROR,
+  FETCH_INTERACTIONS_SUCCESS
+} from './interaction.reducer';
+
+let isNotChecked = (list, pid) => {
+  return !list.find(element => element.book.pid === pid);
+};
+
+export const interactionMiddleware = store => next => action => {
+  switch (action.type) {
+    case INTERACTION: {
+      try {
+        request
+          .post('/v1/object/')
+          .send({
+            _type: INTERACTION,
+            interaction: action.interaction,
+            pid: action.pid,
+            _public: true
+          })
+          .end();
+      } catch (e) {
+        request
+          .post('/v1/log')
+          .send({type: action.type, error: 'CLIENT_LOG_ERROR', msg: String(e)})
+          .end();
+      }
+      return next(action);
+    }
+
+    case FETCH_INTERACTIONS:
+      return (async () => {
+        next(action);
+        try {
+          const dbInteractionData = (await fetchObjects('', 'INTERACTION', 20))
+            .data;
+          let interactions = [];
+          dbInteractionData.forEach(
+            (interactionObj, key) =>
+              (interactions[key] = {
+                type: 'INTERACTION',
+                interaction: interactionObj.interaction,
+                pid: interactionObj.pid
+              })
+          );
+          interactions.reverse();
+          store.dispatch({
+            type: FETCH_INTERACTIONS_SUCCESS,
+            id: action.id,
+            interactions
+          });
+        } catch (e) {
+          store.dispatch({
+            type: FETCH_INTERACTIONS_ERROR,
+            id: action.id
+          });
+        }
+      })();
+
+    case ON_LOCATION_CHANGE: {
+      let pidPath = action.path;
+      let pid = pidPath;
+      if (pidPath.startsWith('/værk/')) {
+        pid = pidPath.slice(6, pidPath.length);
+      }
+      store.dispatch({
+        type: INTERACTION,
+        pid: pid,
+        interaction: 'NEW_LOCATION'
+      });
+      return next(action);
+    }
+
+    case ON_SHORTLIST_TOGGLE_ELEMENT: {
+      // HUSK button is clicked
+      const notChecked = isNotChecked(
+        store.getState().shortListReducer.elements,
+        action.element.book.pid
+      );
+      if (notChecked) {
+        store.dispatch({
+          type: INTERACTION,
+          pid: action.element.book.pid,
+          interaction: 'SHORTLIST'
+        });
+      }
+      return next(action);
+    }
+
+    case ORDER: {
+      store.dispatch({
+        type: INTERACTION,
+        pid: action.book.pid,
+        interaction: 'ORDER'
+      });
+      return next(action);
+    }
+
+    case LIST_TOGGLE_ELEMENT: {
+      const notChecked = isNotChecked(
+        store.getState().listReducer.lists[action.id].list,
+        action.element.book.pid
+      );
+      if (notChecked) {
+        store.dispatch({
+          type: INTERACTION,
+          pid: action.element.book.pid,
+          interaction: 'LIST_TOGGLE_ELEMENT'
+        });
+      }
+      return next(action);
+    }
+
+    default:
+      return next(action);
+  }
+};
---
+++ src/client/redux/interaction.reducer.js
@@ -0,0 +1,46 @@
+export const INTERACTION = 'INTERACTION';
+export const FETCH_INTERACTIONS = 'FETCH_INTERACTIONS';
+export const FETCH_INTERACTIONS_SUCCESS = 'FETCH_INTERACTIONS_SUCCESS';
+export const FETCH_INTERACTIONS_ERROR = 'FETCH_INTERACTIONS_ERROR';
+
+const defaultState = {
+  interactions: []
+};
+
+const interactionReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case INTERACTION: {
+      const newState = Object.assign({}, state);
+      if (state.interactions.length > 0) {
+        newState.interactions.push({
+          type: 'INTERACTION',
+          interaction: action.interaction,
+          pid: action.pid
+        });
+      }
+      return newState;
+    }
+
+    case FETCH_INTERACTIONS: {
+      return Object.assign({}, state, {...state.interactions, isLoading: true});
+    }
+
+    case FETCH_INTERACTIONS_SUCCESS: {
+      return Object.assign({}, state, {
+        interactions: action.interactions,
+        isLoading: false
+      });
+    }
+
+    case FETCH_INTERACTIONS_ERROR: {
+      return Object.assign({}, state, {
+        error: action.error
+      });
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default interactionReducer;
