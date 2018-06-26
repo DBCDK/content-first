f1525743452806c6b96206547a9174db73212ebf
Kasper Fuglsang <kfu@dbc.dk>
2018-06-26 11:42:21 +0200

    added hover overlay for WorkCard

--- src/client/components/work/WorkCard.container.js
+++ src/client/components/work/WorkCard.container.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import BookCover from '../general/BookCover.component';
 import BookmarkButton from '../general/BookmarkButton';
+import Heading from '../base/Heading';
+import Button from '../base/Button';
 import {BOOKS_REQUEST} from '../../redux/books.reducer';
 import Link from '../general/Link.component';
 import Paragraph from '../base/Paragraph';
@@ -74,6 +76,39 @@ class WorkCard extends React.Component {
         <Paragraph className="mt1 d-xs-none d-sm-block">
           {tax_description}
         </Paragraph>
+
+        <div
+          className="hover-details d-xs-none d-sm-block"
+          style={{
+            position: 'absolute',
+            left: 0,
+            bottom: 0,
+            height: '100%',
+            width: '100%',
+            paddingTop: '80%'
+          }}
+          onClick={() => {}}
+        >
+          <Heading tag="h3" type="title" style={{marginBottom: 4}}>
+            {this.props.work.book.title}
+          </Heading>
+          <Heading tag="h3" type="subtitle" className="mt0">
+            {this.props.work.book.creator}
+          </Heading>
+          <Button
+            type="tertiary"
+            size="small"
+            onClick={event => {
+              event.stopPropagation();
+            }}
+          >
+            Mere som denne
+          </Button>
+          <Paragraph className="mt1">{tax_description}</Paragraph>
+          <div className="expand-more-wrapper text-center">
+            <i className="expand-more material-icons md-36">expand_more</i>
+          </div>
+        </div>
         <BookmarkButton
           origin={this.props.origin}
           work={this.props.work}
