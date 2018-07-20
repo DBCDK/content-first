39c22d8df5337ed4994b38dfd45d739508270d0c
Yousef Mohsen <yousefmohsen@outlook.dk>
2018-07-20 13:08:16 +0200

    login-modal small screen view

--- src/client/components/modals/LoginModal.component.js
+++ src/client/components/modals/LoginModal.component.js
@@ -2,27 +2,36 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {HISTORY_PUSH_FORCE_REFRESH} from '../../redux/middleware';
 import {CLOSE_MODAL} from '../../redux/modal.reducer';
-import Kryds from '../svg/Kryds.svg';
-import Link from '../general/Link.component';
+import Kryds from '../svg/KrydsElm.svg';
+import ArrowBack from '../svg/ArrowBack.svg';
+import {isMobile} from 'react-device-detect';
 
 export function LoginModal({context, closeModal, login}) {
-  console.log(context);
-  console.log(closeModal);
-
+  function renderHeder() {
+    if (isMobile) {
+      return (
+        <div className="login-modal-small-screen-header" onClick={closeModal}>
+          <img src={ArrowBack} alt="luk" onClick={closeModal} />
+          <p>Tilbage</p>
+        </div>
+      );
+    }
+    return (
+      <img
+        src={Kryds}
+        alt="luk"
+        className="modal-window--close-btn"
+        onClick={closeModal}
+      />
+    );
+  }
   return (
-    <div className="modal-container ">
+    <div className="modal-container login-modal-container">
       <div className="modal-backdrop" onClick={closeModal} />
       <div className={`modal-window text-left modal-narrow login-modal`}>
-        <img
-          src={Kryds}
-          alt="luk"
-          className="modal-window--close-btn"
-          onClick={closeModal}
-        />
-        <div className="modal-window--header text-center" />
+        <div className="modal-window--header text-center">{renderHeder()}</div>
         <div className="modal-window--content">
           <h3>{context.title || context}</h3>
-
           <p>
             <strong>{context.reason || 'Du skal logge ind.'}</strong>
           </p>
@@ -34,10 +43,8 @@ export function LoginModal({context, closeModal, login}) {
           <small>
             <p>
               Har du ikke en profil? Du kan nemt oprette en profil med det
-              login, du bruger på biblioteket.<a href={'/v1/login'}>
-                {' '}
-                Opret en profil
-              </a>
+              login, du bruger på biblioteket.{' '}
+              <a href={'/v1/login'}>Opret en profil</a>
             </p>
           </small>
         </div>
