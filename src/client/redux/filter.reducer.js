fdd91ee9c084fb0c880eee714ced8d13969ffaf6
mtni <36037828+mtni@users.noreply.github.com>
2018-09-12 11:06:11 +0200

    changed filter card names + content (#654)

--- src/client/redux/filter.reducer.js
+++ src/client/redux/filter.reducer.js
@@ -3,33 +3,6 @@ const leavesMap = getLeavesMap();
 
 export const filters = {
   Stemning: taxonomy.stemning,
-  // Stemning: [
-  //   leavesMap[5636],
-  //   leavesMap[5637],
-  //   leavesMap[5647],
-  //   leavesMap[5648],
-  //   leavesMap[5660],
-  //   leavesMap[5661],
-  //   leavesMap[5657],
-  //   leavesMap[5635],
-  //   leavesMap[5663],
-  //   leavesMap[5667],
-  //   leavesMap[5670],
-  //   leavesMap[5672],
-  //   leavesMap[5676],
-  //   leavesMap[5680],
-  //   leavesMap[5688],
-  //   leavesMap[5681],
-  //   leavesMap[5699],
-  //   leavesMap[5701],
-  //   leavesMap[5700],
-  //   leavesMap[5705],
-  //   leavesMap[5708],
-  //   leavesMap[5712],
-  //   leavesMap[5735],
-  //   leavesMap[5721],
-  //   leavesMap[5725]
-  // ],
   Længde: [
     {id: 100000, title: 'Kort', custom: true},
     {id: 100001, title: 'Medium længde', custom: true},
@@ -41,23 +14,29 @@ export const filters = {
     {id: -2, title: 'Bibliotekaren anbefaler', custom: true}
   ],
   Tempo: taxonomy.fortælleteknik.tempo,
+  Fortællerstemme: taxonomy.fortælleteknik.fortællerstemme,
   'Handlingens tid': taxonomy.ramme['Handlingens tid i ord'],
-  // 'Handlingens tid': [
-  //   leavesMap[5734],
-  //   leavesMap[4995],
-  //   leavesMap[4970],
-  //   leavesMap[5169],
-  //   leavesMap[5016],
-  //   leavesMap[4961]
-  // ],
   Univers: taxonomy.ramme.univers,
-  Struktur: [
+  Skrivestil: [
+    leavesMap[5618],
+    leavesMap[5619],
+    leavesMap[5620],
     leavesMap[5621],
+    leavesMap[5622],
     leavesMap[5623],
-    leavesMap[5625],
-    leavesMap[5624]
+    leavesMap[5624],
+    leavesMap[5625]
   ],
-  Skrivestil: [leavesMap[5611], leavesMap[5610], leavesMap[5612]]
+  Sprog: [
+    leavesMap[5611],
+    leavesMap[5610],
+    leavesMap[5612],
+    leavesMap[5616],
+    leavesMap[5613],
+    leavesMap[5614],
+    leavesMap[5615],
+    leavesMap[5617]
+  ]
 };
 export const filtersMap = getLeavesMap(filters);
 export const filtersMapAll = {...getLeavesMap(), ...filtersMap}; // all tags from taxonomy
