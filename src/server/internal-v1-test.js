b5e12a59a2f39196c35b105cc04e0788d5ef55ea
Yousef Mohsen <yousefmohsen@outlook.dk>
2018-11-19 12:58:07 +0100

    test-login endpoint splitted into /create & /login

--- src/server/internal-v1-test.js
+++ src/server/internal-v1-test.js
@@ -14,47 +14,13 @@ const ms_OneMonth = 30 * 24 * 60 * 60 * 1000;
 
 router
   .route('/login/:id')
-  //
+  // Log in only and create user manually
   // GET /v1/test/login/:id
   //
   .get(
     asyncMiddleware(async (req, res) => {
-      const loginToken = uuidv4();
-      const openplatformId = req.params.id;
-      const openplatformToken = req.params.id;
-
       try {
-        let userId = -1;
-        await knex(cookieTable).insert({
-          cookie: loginToken,
-          community_profile_id: userId,
-          openplatform_id: openplatformId,
-          openplatform_token: openplatformToken,
-          expires_epoch_s: Math.ceil((Date.now() + ms_OneMonth) / 1000)
-        });
-        req.cookies['login-token'] = loginToken;
-        if (
-          (await objectStore.find({
-            type: 'USER_PROFILE',
-            owner: openplatformId,
-            limit: 1
-          })).data.length === 0
-        ) {
-          // create user in db if user dosn't exist
-          await putUserData(
-            {
-              name: '',
-              roles: [],
-              openplatformId,
-              shortlist: [],
-              profiles: [],
-              lists: [],
-              acceptedAge: false,
-              acceptedTerms: false
-            },
-            req
-          );
-        }
+        const loginToken = await createUser(req, false);
         return res
           .status(303)
           .location('http://localhost:3000/replay')
@@ -74,47 +40,14 @@ router
 
 router
   .route('/create/:id')
-  // Create user and log in
+  // Log in and create user
   // GET /v1/test/create/:id
   //
   .get(
     asyncMiddleware(async (req, res) => {
-      const loginToken = uuidv4();
-      const openplatformId = req.params.id;
-      const openplatformToken = req.params.id;
-
       try {
-        let userId = -1;
-        await knex(cookieTable).insert({
-          cookie: loginToken,
-          community_profile_id: userId,
-          openplatform_id: openplatformId,
-          openplatform_token: openplatformToken,
-          expires_epoch_s: Math.ceil((Date.now() + ms_OneMonth) / 1000)
-        });
-        req.cookies['login-token'] = loginToken;
-        if (
-          (await objectStore.find({
-            type: 'USER_PROFILE',
-            owner: openplatformId,
-            limit: 1
-          })).data.length === 0
-        ) {
-          // create user in db if user dosn't exist
-          await putUserData(
-            {
-              name: openplatformId,
-              roles: [],
-              openplatformId,
-              shortlist: [],
-              profiles: [],
-              lists: [],
-              acceptedAge: true,
-              acceptedTerms: true
-            },
-            req
-          );
-        }
+        const loginToken = await createUser(req, true);
+
         return res
           .status(303)
           .location('http://localhost:3000/replay')
@@ -131,5 +64,49 @@ router
       }
     })
   );
+
+/**
+ *
+ * @param {String} id openplatform_id
+ * @param {boolean} createUser   if true: create user profile and log in. Else only log in
+ */
+async function createUser(req, doCreateUser) {
+  const loginToken = uuidv4();
+
+  const id = req.params.id;
+
+  await knex(cookieTable).insert({
+    cookie: loginToken,
+    community_profile_id: -1,
+    openplatform_id: id,
+    openplatform_token: id,
+    expires_epoch_s: Math.ceil((Date.now() + ms_OneMonth) / 1000)
+  });
+  req.cookies['login-token'] = loginToken;
+  if (
+    (await objectStore.find({
+      type: 'USER_PROFILE',
+      owner: id,
+      limit: 1
+    })).data.length === 0
+  ) {
+    // create user in db if user dosn't exist
+    await putUserData(
+      {
+        name: doCreateUser ? id : '',
+        roles: [],
+        openplatformId: id,
+        shortlist: [],
+        profiles: [],
+        lists: [],
+        acceptedAge: doCreateUser ? true : false,
+        acceptedTerms: doCreateUser ? true : false
+      },
+      req
+    );
+  }
+
+  return loginToken;
+}
 
 module.exports = router;
