a11b2d7c81a8e13609ec7bdca987c2bdf738f8db
Stanford Vinson <stvi@dbc.dk>
2019-03-06 10:49:19 +0100

    prettier

--- src/client/components/filter/SearchBar.component.js
+++ src/client/components/filter/SearchBar.component.js
@@ -8,165 +8,162 @@ import {TOGGLE_FILTER} from '../../redux/filter.reducer';
 import {HISTORY_REPLACE} from '../../redux/middleware';
 import {BOOKS_REQUEST} from '../../redux/books.reducer';
 import {
-	getRecommendedBooks,
-	getTagsFromUrl,
-	getCreatorsFromUrl,
-	getTitlesFromUrl,
-	getIdsFromRange,
-	getTagsbyIds
+  getRecommendedBooks,
+  getTagsFromUrl,
+  getCreatorsFromUrl,
+  getTitlesFromUrl,
+  getIdsFromRange,
+  getTagsbyIds
 } from '../../redux/selectors';
 import {REORGANIZE_FILTERPAGE_BELTS} from '../../redux/belts.reducer';
 
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-	constructor() {
-		super();
-		this.state = {query: '', expanded: false};
-	}
+  constructor() {
+    super();
+    this.state = {query: '', expanded: false};
+  }
 
-	componentDidMount() {
-		this.initFilterPosition();
-	}
+  componentDidMount() {
+    this.initFilterPosition();
+  }
 
-	toggleFilter(filterId) {
-		this.props.toggleFilter(filterId);
-	}
+  toggleFilter(filterId) {
+    this.props.toggleFilter(filterId);
+  }
 
-	triggerCancelToast(historyPath, historyParams) {
-		toast(
-			<ToastMessage
-				type="info"
-				icon="history"
-				lines={[
-					T({component: 'filter', name: 'newSearchToast'}),
-					<a
-						onClick={() =>
-							this.props.historyReplace(historyPath, historyParams)
-						}
-					>
-						<T component="general" name="back" />
-					</a>
-				]}
-			/>,
-			{hideProgressBar: false, pauseOnHover: true}
-		);
-	}
+  triggerCancelToast(historyPath, historyParams) {
+    toast(
+      <ToastMessage
+        type="info"
+        icon="history"
+        lines={[
+          T({component: 'filter', name: 'newSearchToast'}),
+          <a
+            onClick={() =>
+              this.props.historyReplace(historyPath, historyParams)
+            }
+          >
+            <T component="general" name="back" />
+          </a>
+        ]}
+      />,
+      {hideProgressBar: false, pauseOnHover: true}
+    );
+  }
 
-	onFiltersMouseWheelScrool(e) {
-		e.preventDefault();
-		let scrollSpeed = 40;
-		/* eslint-disable no-unused-expressions */
-		e.deltaY > 0
-			? (this.filtersRef.scrollLeft += scrollSpeed)
-			: (this.filtersRef.scrollLeft -= scrollSpeed);
-		/* eslint-enable no-unused-expressions */
-	}
+  onFiltersMouseWheelScrool(e) {
+    e.preventDefault();
+    let scrollSpeed = 40;
+    /* eslint-disable no-unused-expressions */
+    e.deltaY > 0
+      ? (this.filtersRef.scrollLeft += scrollSpeed)
+      : (this.filtersRef.scrollLeft -= scrollSpeed);
+    /* eslint-enable no-unused-expressions */
+  }
 
-	initFilterPosition() {
-		if (this.filtersRef) {
-			this.filtersRef.scrollLeft = 99999999;
-		}
-	}
+  initFilterPosition() {
+    if (this.filtersRef) {
+      this.filtersRef.scrollLeft = 99999999;
+    }
+  }
 
-	handleOnKeyDown(e) {
-		let tags = this.props.selectedTagIds;
-		/* Hvis der er tags i url */
+  handleOnKeyDown(e) {
+    let tags = this.props.selectedTagIds;
+    /* Hvis der er tags i url */
 
-		if (tags.length > 0) {
+    if (tags.length > 0) {
+      /* Hvis brugeren trykker backspace */
+      if (e.keyCode === 8) {
+        /* Hvis brugeren ikke er igang med at skrive et ord */
+        if (this.state.query === '') {
+          this.props.toggleFilter(tags[tags.length - 1]);
+          this.initFilterPosition();
+        }
+      }
+    }
+  }
 
-			/* Hvis brugeren trykker backspace */
-			if (e.keyCode === 8) {
-				/* Hvis brugeren ikke er igang med at skrive et ord */
-				if (this.state.query === '') {
-					this.props.toggleFilter(tags[tags.length - 1]);
-					this.initFilterPosition();
-				}
-			}
-		}
-	}
-
-	render() {
-
-		return (
-			<SelectedFilters
-				filtersRef={r => {
-					this.filtersRef = r;
-				}}
-				onFiltersScroll={e => this.onFiltersMouseWheelScrool(e)}
-				selectedFilters={this.props.selectedTags}
-				selectedTags={this.props.selectedTagIds}
-				transition={this.props.transition}
-				filters={this.props.filters}
-				edit={this.state.expanded}
-				onEditFilterToggle={this.props.editFilterToggle}
-				query={this.state.query}
-				onQueryChange={e => this.setState({query: e.target.value})}
-				onFilterToggle={filter => {
-					this.toggleFilter(filter.id || filter);
-					if (this.props.checkForTags) {
-						this.props.checkForTags();
-					}
-
-				}}
-				onKeyDown={e => this.handleOnKeyDown(e)}
-				onFocus={() => {
-					this.setState({expanded: true});
-					this.initFilterPosition();
-				}}
-			/>
-		);
-	}
+  render() {
+    return (
+      <SelectedFilters
+        filtersRef={r => {
+          this.filtersRef = r;
+        }}
+        onFiltersScroll={e => this.onFiltersMouseWheelScrool(e)}
+        selectedFilters={this.props.selectedTags}
+        selectedTags={this.props.selectedTagIds}
+        transition={this.props.transition}
+        filters={this.props.filters}
+        edit={this.state.expanded}
+        onEditFilterToggle={this.props.editFilterToggle}
+        query={this.state.query}
+        onQueryChange={e => this.setState({query: e.target.value})}
+        onFilterToggle={filter => {
+          this.toggleFilter(filter.id || filter);
+          if (this.props.checkForTags) {
+            this.props.checkForTags();
+          }
+        }}
+        onKeyDown={e => this.handleOnKeyDown(e)}
+        onFocus={() => {
+          this.setState({expanded: true});
+          this.initFilterPosition();
+        }}
+      />
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	const filterCards = state.filtercardReducer;
-	const selectedTagIds = getTagsFromUrl(state);
-	const selectedCreators = getCreatorsFromUrl(state);
-	const selectedTitles = getTitlesFromUrl(state);
-	const plainSelectedTagIds = getIdsFromRange(state, selectedTagIds);
-	const selectedTags = getTagsbyIds(state, selectedTagIds);
-	const mergedSelectedTags = [].concat(
-		selectedTagIds,
-		selectedCreators,
-		selectedTitles
-	);
+  const filterCards = state.filtercardReducer;
+  const selectedTagIds = getTagsFromUrl(state);
+  const selectedCreators = getCreatorsFromUrl(state);
+  const selectedTitles = getTitlesFromUrl(state);
+  const plainSelectedTagIds = getIdsFromRange(state, selectedTagIds);
+  const selectedTags = getTagsbyIds(state, selectedTagIds);
+  const mergedSelectedTags = [].concat(
+    selectedTagIds,
+    selectedCreators,
+    selectedTitles
+  );
 
-	return {
-		recommendedPids: getRecommendedBooks(state, plainSelectedTagIds, 300),
-		filterCards,
-		router: state.routerReducer,
-		transition: state.routerReducer.params.transition || false,
-		selectedTagIds: mergedSelectedTags,
-		plainSelectedTagIds,
-		selectedTags: selectedTags.length > 0 ? selectedTags : mergedSelectedTags,
-		filters: state.filterReducer.filters,
-		editFilters: state.filterReducer.editFilters,
-		expandedFilters: state.filterReducer.expandedFilters
-	};
+  return {
+    recommendedPids: getRecommendedBooks(state, plainSelectedTagIds, 300),
+    filterCards,
+    router: state.routerReducer,
+    transition: state.routerReducer.params.transition || false,
+    selectedTagIds: mergedSelectedTags,
+    plainSelectedTagIds,
+    selectedTags: selectedTags.length > 0 ? selectedTags : mergedSelectedTags,
+    filters: state.filterReducer.filters,
+    editFilters: state.filterReducer.editFilters,
+    expandedFilters: state.filterReducer.expandedFilters
+  };
 };
 export const mapDispatchToProps = dispatch => ({
-	toggleFilter: id => dispatch({type: TOGGLE_FILTER, id}),
-	historyReplace: (path, params) => {
-		dispatch({
-			type: HISTORY_REPLACE,
-			path,
-			params
-		});
-	},
-	onSearch: query =>
-		dispatch({type: 'SEARCH_QUERY', query: query.toLowerCase()}),
-	fetchWorks: pids =>
-		dispatch({
-			type: BOOKS_REQUEST,
-			pids: pids
-		}),
-	reorganizeBelts: () => {
-		dispatch({type: REORGANIZE_FILTERPAGE_BELTS});
-	}
+  toggleFilter: id => dispatch({type: TOGGLE_FILTER, id}),
+  historyReplace: (path, params) => {
+    dispatch({
+      type: HISTORY_REPLACE,
+      path,
+      params
+    });
+  },
+  onSearch: query =>
+    dispatch({type: 'SEARCH_QUERY', query: query.toLowerCase()}),
+  fetchWorks: pids =>
+    dispatch({
+      type: BOOKS_REQUEST,
+      pids: pids
+    }),
+  reorganizeBelts: () => {
+    dispatch({type: REORGANIZE_FILTERPAGE_BELTS});
+  }
 });
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(SearchBar);
--- src/client/components/filter/SelectedFilters.component.js
+++ src/client/components/filter/SelectedFilters.component.js
@@ -4,71 +4,70 @@ import Icon from '../base/Icon';
 import Button from '../base/Button';
 
 const SelectedFilter = props => {
-	let title = props.filter.title || props.filter;
-	let id = props.filter;
+  let title = props.filter.title || props.filter;
+  let id = props.filter;
 
-	if (props.filter instanceof Array) {
-		/* Capitalize first letter for each in range */
-		const first = props.filter[0].title.replace(/^\w/, c => c.toUpperCase());
-		const last = props.filter[1].title.replace(/^\w/, c => c.toUpperCase());
+  if (props.filter instanceof Array) {
+    /* Capitalize first letter for each in range */
+    const first = props.filter[0].title.replace(/^\w/, c => c.toUpperCase());
+    const last = props.filter[1].title.replace(/^\w/, c => c.toUpperCase());
 
-		title = first === last ? first : first + ' - ' + last;
-		id = [props.filter[0].id, props.filter[1].id];
-	}
+    title = first === last ? first : first + ' - ' + last;
+    id = [props.filter[0].id, props.filter[1].id];
+  }
 
-	if (typeof props.filter === 'string' || props.filter instanceof String) {
-		id = {text: props.filter, parents: ['', 'Forfatter']};
-	}
+  if (typeof props.filter === 'string' || props.filter instanceof String) {
+    id = {text: props.filter, parents: ['', 'Forfatter']};
+  }
 
-	return (
-		<Button Tag="div" size="medium" type="term" className={`selected-filter`}>
-			<span>{title}</span>
-			<Icon
-				className="md-small"
-				name="close"
-				onClick={() => props.onDisableFilter(id)}
-			/>
-		</Button>
-	);
+  return (
+    <Button Tag="div" size="medium" type="term" className={`selected-filter`}>
+      <span>{title}</span>
+      <Icon
+        className="md-small"
+        name="close"
+        onClick={() => props.onDisableFilter(id)}
+      />
+    </Button>
+  );
 };
 
 class SelectedFilters extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<div
-					className="selected-filters-wrap text-left"
-					id="selected-filters-wrap"
-					ref={this.props.filtersRef}
-					onWheel={this.props.onFiltersScroll}
-				>
-					<TagsSuggester
-
-						selectedFilters={this.props.selectedFilters}
-						filters={this.props.filters}
-						value={this.props.query}
-						onKeyDown={this.props.onKeyDown}
-						onFocus={this.props.onFocus}
-						onChange={this.props.onQueryChange}
-						onSuggestionSelected={(e, {suggestion}) =>
-							this.props.onFilterToggle(suggestion)
-						}
-					/>
-					<div id="selectedFilters" className="selected-filters">
-						{this.props.selectedFilters.map((filter, idx) => {
-							return (
-								<SelectedFilter
-									key={idx}
-									filter={filter}
-									onDisableFilter={this.props.onFilterToggle}
-								/>
-							);
-						})}
-					</div>
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <div
+          className="selected-filters-wrap text-left"
+          id="selected-filters-wrap"
+          ref={this.props.filtersRef}
+          onWheel={this.props.onFiltersScroll}
+        >
+          <TagsSuggester
+            selectedFilters={this.props.selectedFilters}
+            filters={this.props.filters}
+            value={this.props.query}
+            onKeyDown={this.props.onKeyDown}
+            onFocus={this.props.onFocus}
+            onChange={this.props.onQueryChange}
+            onSuggestionSelected={(e, {suggestion}) =>
+              this.props.onFilterToggle(suggestion)
+            }
+          />
+          <div id="selectedFilters" className="selected-filters">
+            {this.props.selectedFilters.map((filter, idx) => {
+              return (
+                <SelectedFilter
+                  key={idx}
+                  filter={filter}
+                  onDisableFilter={this.props.onFilterToggle}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default SelectedFilters;
