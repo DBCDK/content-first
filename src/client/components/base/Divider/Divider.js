85daa04f330cae64adba9c842e9c8e7271ba024f
Steen Lillethorup Frederiksen <slf@dbc.dk>
2019-03-13 11:39:56 +0100

    Handle undefined class name

--- src/client/components/base/Divider/Divider.js
+++ src/client/components/base/Divider/Divider.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Divider.css';
 
 const Divider = props => {
-  var newProps = {...props, className: 'Divider ' + props.className};
+  var newProps = {
+    ...props,
+    className:
+      typeof props.className === 'undefined'
+        ? 'Divider'
+        : 'Divider ' + props.className
+  };
   return <div {...newProps} />;
 };
 
