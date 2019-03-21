5610c219ddc64e2c285d0f30b53817196bcd8d84
mtni <36037828+mtni@users.noreply.github.com>
2019-03-21 10:36:26 +0100

    Footer i fuld bredde (#920)
    * footer full-width done
    
    * footer done + test done

--- src/client/components/general/Footer.component.js
+++ src/client/components/general/Footer.component.js
@@ -8,83 +8,88 @@ import LaesekompasLogo from '../svg/LaesekompasLogo.svg';
 import './Footer.css';
 
 class Footer extends React.Component {
-  state = {height: 0};
-  footerContainer = React.createRef();
-
-  setFooterHeight = () => {
-    if (this.footerContainer && this.footerContainer.current) {
-      this.setState({height: this.footerContainer.current.offsetHeight});
-    } else {
-      this.setState({height: 0});
-    }
-  };
+  constructor(props) {
+    super(props);
+    this.state = {sticky: null};
+  }
 
   componentDidMount() {
-    this.setFooterHeight();
-    window.addEventListener('resize', this.setFooterHeight);
+    this.handleStickyState();
+    window.addEventListener('resize', this.handleStickyState);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.setFooterHeight);
+    window.removeEventListener('resize', this.handleStickyState);
+  }
+
+  componentDidUpdate() {
+    // check sticky state now
+    this.handleStickyState();
+    // Ensure sticky state
+    setTimeout(() => {
+      this.handleStickyState();
+    }, 100);
+  }
+
+  handleStickyState = () => {
+    const sticky = this.shouldBeSticky();
+    if (sticky !== this.state.sticky) {
+      this.setState({sticky});
+    }
+  };
+
+  shouldBeSticky() {
+    return !(document.body.scrollHeight - window.innerHeight);
   }
 
   render() {
+    const sticky = this.state.sticky;
+    const isStickyClass = sticky ? 'Footer__sticky' : '';
+
+    if (sticky === null) {
+      return null;
+    }
+
     return (
-      <React.Fragment>
-        <div
-          className="Footer__container--spacer"
-          style={{height: this.state.height + 'px'}}
-        />
-        <div className="row Footer__outer-container--flexbox">
-          <div
-            className="row Footer__container--elements"
-            ref={this.footerContainer}
-          >
-            <div className="Footer__logo--element">
-              <img src={LaesekompasLogo} className="Footer__logo--image" />
-            </div>
-            <div className="col Footer__element--block">
-              <Text type="body" className="mt1">
-                <T component="footer" name="sectionOne" />
-                <br />
-                <Link href="/om">
-                  <T component="general" name="readMore" />
-                </Link>
-              </Text>
-            </div>
-            <div className="col Footer__element--block">
-              <Text type="body" className="mt1">
-                <T component="footer" name="sectionTwo" />
-              </Text>
-            </div>
-            <div className="col Footer__element--block">
-              <Text type="body" className="mt1">
-                <T component="footer" name="customerServiceText" />
-                <br />
-                <a href="https://kundeservice.dbc.dk" target="_blank">
-                  <T component="footer" name="customerServiceLinkText" />
-                </a>
-                <br />
-                <br />
-                <T component="footer" name="writeToManagementText" />
-                <br />
-                <a href="mailto:laesekompasset@dbc.dk">
-                  <T component="footer" name="writeToManagementLinkText" />
-                </a>
-              </Text>
-            </div>
-            <div className="col Footer__element--block">
-              <Text type="body" className="mt1">
-                <T component="footer" name="addressCompany" />
-                <br />
-                <T component="footer" name="addressStreet" />
-                <br />
-                <T component="footer" name="addressCity" />
-              </Text>
-            </div>
+      <div className={`Footer__outer-container--flexbox ${isStickyClass}`}>
+        <div className="Footer__container--elements p-3 pt-5 pb-md-5">
+          <div className="Footer__logo--element mb-1">
+            <img src={LaesekompasLogo} className="Footer__logo--image" />
+          </div>
+          <div className="Footer__element--block">
+            <Text type="body" className="mt-3 pr-md-3 pr-lg-3">
+              <T component="footer" name="sectionOne" />
+              <br />
+              <Link href="/om">
+                <T component="general" name="readMore" />
+              </Link>
+            </Text>
+          </div>
+          <div className="Footer__element--block">
+            <Text type="body" className="mt-3 pr-lg-3">
+              <T component="footer" name="sectionTwo" />
+            </Text>
+          </div>
+          <div className="Footer__element--block">
+            <Text type="body" className="mt-3 pr-md-3 pr-lg-3">
+              <T component="footer" name="customerServiceText" />
+              <br />
+              <a href="https://kundeservice.dbc.dk" target="_blank">
+                <T component="footer" name="customerServiceLinkText" />
+              </a>
+            </Text>
+          </div>
+          <div className="Footer__element--block">
+            <Text type="body" className="mt-3">
+              <T component="footer" name="addressCompany" />
+              <br />
+              <T component="footer" name="addressStreet" />
+              <br />
+              <T component="footer" name="addressCity" />
+            </Text>
           </div>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
