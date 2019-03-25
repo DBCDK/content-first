9933e3bc5777fed46e819cd438938154090659b5
Stanford Vinson <stvi@dbc.dk>
2019-03-25 16:36:36 +0100

    rebase master

--- src/client/components/general/HoverImage.component.js
+++ src/client/components/general/HoverImage.component.js
@@ -1,39 +1,39 @@
 import React from 'react';
 
 export default class HoverImage extends React.Component {
-  mouseEnter = () => {
-    this.setState({src: this.props.mouseOver});
-  };
-  mouseLeave = () => {
-    this.setState({src: this.props.src});
-  };
+	mouseEnter = () => {
+		this.setState({src: this.props.mouseOver});
+	};
+	mouseLeave = () => {
+		this.setState({src: this.props.src});
+	};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      src: props.src
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			src: props.src
+		};
+	}
 
-  componentDidMount() {
-    this.setState({src: this.props.src});
-  }
+	componentDidMount() {
+		this.setState({src: this.props.src});
+	}
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.src !== this.props.src) {
-      this.mouseEnter();
-      this.mouseLeave();
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (prevProps.src !== this.props.src) {
+			this.mouseEnter();
+			this.mouseLeave();
+		}
+	}
 
-  render() {
-    return (
-      <img
-        className="image-upload-image"
-        src={this.state.src}
-        onMouseOver={this.mouseEnter}
-        onMouseOut={this.mouseLeave}
-      />
-    );
-  }
+	render() {
+		return (
+			<img
+				className="image-upload-image"
+				src={this.state.src}
+				onMouseOver={this.mouseEnter}
+				onMouseOut={this.mouseLeave}
+			/>
+		);
+	}
 }
--- src/client/components/profile/CreateProfilePage.js
+++ src/client/components/profile/CreateProfilePage.js
@@ -9,151 +9,151 @@ import {toast} from 'react-toastify';
 import ToastMessage from '../base/ToastMessage';
 
 import {
-  ADD_PROFILE_IMAGE,
-  SAVE_USER_PROFILE,
-  DELETE_USER_PROFILE
+	ADD_PROFILE_IMAGE,
+	SAVE_USER_PROFILE,
+	DELETE_USER_PROFILE
 } from '../../redux/user.reducer';
 
 export class CreateProfilePage extends React.Component {
-  activateSaveButton = () => {
-    this.setState({enableButton: true});
-  };
+	activateSaveButton = () => {
+		this.setState({enableButton: true});
+	};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      enableButton: false
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			enableButton: false
+		};
+	}
 
-  render() {
-    const showImageSection = () => {
-      return (
-        <div className="profile__edit-image-zone">
-          <div className="profile__edit-image-upload">
-            <ProfileUploadImage
-              error={this.props.imageError}
-              loading={this.props.imageIsLoading}
-              personalImage={
-                this.props.profileImageId
-                  ? `/v1/image/${this.props.profileImageId}/150/150`
-                  : null
-              }
-              tempPersonalImage={
-                this.props.tempImageId
-                  ? `/v1/image/${this.props.tempImageId}/150/150`
-                  : null
-              }
-              onFile={this.props.addImage}
-              thumbnailImage={'/img/general/user-placeholder-thumbnail.svg'}
-              thumbnailImageHover={
-                '/img/general/user-placeholder-thumbnail-hover.svg'
-              }
-              activateSaveButton={this.activateSaveButton}
-            />
-          </div>
-          <div className="profile__edit-image-text">Vælg billede</div>
-        </div>
-      );
-    };
-    const showFormSection = () => {
-      return (
-        <div className="profile__edit-textfield-zone">
-          <EditProfileForm
-            name={this.props.name}
-            agencyName={this.props.agencyName}
-            updateImageId={this.props.addImage}
-            imageId={this.props.profileImageId}
-            confirmDelete={() => this.props.confirmDeleteModal()}
-            updateProfile={this.props.saveUser}
-            isSaving={this.props.isSaving}
-            activateSaveButton={this.activateSaveButton}
-            enableButton={this.state.enableButton}
-          />
-        </div>
-      );
-    };
+	render() {
+		const showImageSection = () => {
+			return (
+				<div className="profile__edit-image-zone">
+					<div className="profile__edit-image-upload">
+						<ProfileUploadImage
+							error={this.props.imageError}
+							loading={this.props.imageIsLoading}
+							personalImage={
+								this.props.profileImageId
+									? `/v1/image/${this.props.profileImageId}/150/150`
+									: null
+							}
+							tempPersonalImage={
+								this.props.tempImageId
+									? `/v1/image/${this.props.tempImageId}/150/150`
+									: null
+							}
+							onFile={this.props.addImage}
+							thumbnailImage={'/img/general/user-placeholder-thumbnail.svg'}
+							thumbnailImageHover={
+								'/img/general/user-placeholder-thumbnail-hover.svg'
+							}
+							activateSaveButton={this.activateSaveButton}
+						/>
+					</div>
+					<div className="profile__edit-image-text">Vælg billede</div>
+				</div>
+			);
+		};
+		const showFormSection = () => {
+			return (
+				<div className="profile__edit-textfield-zone">
+					<EditProfileForm
+						name={this.props.name}
+						agencyName={this.props.agencyName}
+						updateImageId={this.props.addImage}
+						imageId={this.props.profileImageId}
+						confirmDelete={() => this.props.confirmDeleteModal()}
+						updateProfile={this.props.saveUser}
+						isSaving={this.props.isSaving}
+						activateSaveButton={this.activateSaveButton}
+						enableButton={this.state.enableButton}
+					/>
+				</div>
+			);
+		};
 
-    const isDeleting = this.props.isDeleting || false;
+		const isDeleting = this.props.isDeleting || false;
 
-    if (isDeleting) {
-      return <Spinner className="spinner-deleteProfile" size="36px" />;
-    }
+		if (isDeleting) {
+			return <Spinner className="spinner-deleteProfile" size="36px" />;
+		}
 
-    return (
-      <div className="container">
-        <div className="profile__edit-titlezone">
-          <h1 className="profile__edit-title">{this.props.title}</h1>
-        </div>
-        <div className="profile__edit-page-position">
-          <div className="profile__edit-top-position">
-            {showImageSection()}
-            {showFormSection()}
-          </div>
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className="container">
+				<div className="profile__edit-titlezone">
+					<h1 className="profile__edit-title">{this.props.title}</h1>
+				</div>
+				<div className="profile__edit-page-position">
+					<div className="profile__edit-top-position">
+						{showImageSection()}
+						{showFormSection()}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export const mapStateToProps = state => ({
-  name: state.userReducer.name,
-  isLoading: state.userReducer.isLoading,
-  isSaving: state.userReducer.isSaving,
-  isDeleting: state.userReducer.isDeleting,
-  profileImageId: state.userReducer.tempImageId || state.userReducer.image,
-  imageIsLoading: state.userReducer.profileImageIsLoading,
-  error: state.userReducer.saveUserError,
-  imageError: state.userReducer.imageError,
-  agencyName: state.userReducer.agencyName
+	name: state.userReducer.name,
+	isLoading: state.userReducer.isLoading,
+	isSaving: state.userReducer.isSaving,
+	isDeleting: state.userReducer.isDeleting,
+	profileImageId: state.userReducer.tempImageId || state.userReducer.image,
+	imageIsLoading: state.userReducer.profileImageIsLoading,
+	error: state.userReducer.saveUserError,
+	imageError: state.userReducer.imageError,
+	agencyName: state.userReducer.agencyName
 });
 export const mapDispatchToProps = dispatch => {
-  const closeModal = (type, modal) => dispatch({type, modal});
-  const deleteUser = type => dispatch({type});
-  const showToast = () => {
-    toast(
-      <ToastMessage
-        type="success"
-        icon="check_circle"
-        lines={[<T component="profile" name="saveProfileToast" />]}
-      />,
-      {pauseOnHover: true}
-    );
-  };
+	const closeModal = (type, modal) => dispatch({type, modal});
+	const deleteUser = type => dispatch({type});
+	const showToast = () => {
+		toast(
+			<ToastMessage
+				type="success"
+				icon="check_circle"
+				lines={[<T component="profile" name="saveProfileToast" />]}
+			/>,
+			{pauseOnHover: true}
+		);
+	};
 
-  return {
-    addImage: image => dispatch({type: ADD_PROFILE_IMAGE, image}),
-    saveUser: user => {
-      dispatch(
-        {
-          type: SAVE_USER_PROFILE,
-          user
-        },
-        showToast()
-      );
-    },
-    closeModal,
-    confirmDeleteModal: () => {
-      dispatch({
-        type: 'OPEN_MODAL',
-        modal: 'confirm',
-        context: {
-          title: <T component="profile" name="deleteProfileModalTitle" />,
-          reason: (
-            <T component="profile" name="deleteProfileModalDescription" />
-          ),
-          confirmText: <T component="profile" name="deleteMyProfile" />,
-          onConfirm: () => {
-            closeModal('CLOSE_MODAL', 'confirm');
-            deleteUser(DELETE_USER_PROFILE);
-          },
-          onCancel: () => closeModal('CLOSE_MODAL', 'confirm')
-        }
-      });
-    }
-  };
+	return {
+		addImage: image => dispatch({type: ADD_PROFILE_IMAGE, image}),
+		saveUser: user => {
+			dispatch(
+				{
+					type: SAVE_USER_PROFILE,
+					user
+				},
+				showToast()
+			);
+		},
+		closeModal,
+		confirmDeleteModal: () => {
+			dispatch({
+				type: 'OPEN_MODAL',
+				modal: 'confirm',
+				context: {
+					title: <T component="profile" name="deleteProfileModalTitle" />,
+					reason: (
+						<T component="profile" name="deleteProfileModalDescription" />
+					),
+					confirmText: <T component="profile" name="deleteMyProfile" />,
+					onConfirm: () => {
+						closeModal('CLOSE_MODAL', 'confirm');
+						deleteUser(DELETE_USER_PROFILE);
+					},
+					onCancel: () => closeModal('CLOSE_MODAL', 'confirm')
+				}
+			});
+		}
+	};
 };
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(CreateProfilePage);
