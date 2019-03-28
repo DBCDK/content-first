f4d043679e4460fd97dca4f5e3864c4f53a42edb
Steen Lillethorup Frederiksen <slf@dbc.dk>
2019-03-28 15:24:53 +0100

    936 new huskeliste slf (#934)
    * First cut on the new Toolbar component
    
    * Add new componente: Divider
    
    * Handle undefined class name
    
    * Update Styleguide according to new components and updates
    
    * Update Button component to handle left and right aligned icons
    
    * Bugfix: Toolbar cannot handle only one element
    
    * Allow Toolbar to have props
    
    * Small adjustments
    
    * Bugfix: Assure that "Giv os feedback" text is written only once
    
    * More elegant coding
    
    * Simplify code
    
    * Add key to Toolbar list element to eliminate warning
    
    * Enrich OrderButton with left/right icons
    
    * Make Toolbar more robust - test for empty children
    
    * Allow Toobar to accept arrays of elements (as div does)
    
    * Add new prop: href, to give an URL to an external web page instead of onClick
    
    * Missing changes: Mobile styling and correct banner
    
    * Prettier
    
    * Now with correct Banner
    
    * And correct banner text
    
    * Styling of mobile version
    
    * Rewrite the test for Shortlist
    Add new snapshots as a consequence of an added span in Button

--- src/client/components/list/shortlist/ShortList.container.js
+++ src/client/components/list/shortlist/ShortList.container.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import Textarea from 'react-textarea-autosize';
 import {
   ON_SHORTLIST_REMOVE_ELEMENT,
   SHORTLIST_UPDATE_ORIGIN,
@@ -12,159 +11,211 @@ import {ORDER} from '../../../redux/order.reducer';
 import BookCover from '../../general/BookCover.component';
 import OrderButton from '../../order/OrderButton.component';
 import Link from '../../general/Link.component';
+import Toolbar from '../../base/Toolbar';
+import Title from '../../base/Title';
+import Text from '../../base/Text';
 import T from '../../base/T';
+import Divider from '../../base/Divider';
+import Button from '../../base/Button';
+import Banner from '../../base/Banner';
+import {filterCollection} from '../../work/workFunctions';
+import withWork from '../../base/Work/withWork.hoc';
 
 export class ShortListItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      description: props.element.origin
+      description: props.origin
     };
   }
 
   render() {
-    const url = `/værk/${this.props.element.book.pid}`;
+    const {work, pid, origin, className} = this.props;
+    if (!work) {
+      return null;
+    }
+    const url = `/værk/${pid}`;
+
+    // get collections including ereolen
+    const collection = filterCollection(this.props.work);
+    const addToListButton = (
+      <Button
+        align="left"
+        size="medium"
+        type="tertiary"
+        className="mr-2 text-uppercase"
+        onClick={this.props.onAddToList}
+      >
+        <T component="list" name="addToList" />
+      </Button>
+    );
+    const orderBookButton = (
+      <OrderButton
+        book={work.book}
+        align="right"
+        size="medium"
+        type="quaternary"
+        className="ml-2"
+        iconLeft="chrome_reader_mode"
+      >
+        <T component="general" name="book" />
+      </OrderButton>
+    );
+    const orderElectronicBookButtons =
+      work.collectionHasLoaded &&
+      collection.filter(col => col.count === 1).map(col => {
+        return (
+          <Button
+            align="right"
+            size="medium"
+            type="quaternary"
+            iconLeft={col.icon}
+            className="ml-2"
+            key={col.url}
+            href={col.url}
+          >
+            {col.type}
+          </Button>
+        );
+      });
 
     return (
-      <div className="item col-12 col-lg-10 text-left">
-        <div className="row">
-          <div className="book col-6">
-            <div className="row">
-              <div className="col-2">
-                <Link href={url}>
-                  <BookCover
-                    book={this.props.element.book}
-                    style={{width: 'unset'}}
-                  />
-                </Link>
-              </div>
-              <div className="col-10 book-description">
-                <div className="title">
-                  <Link href={url}>{this.props.element.book.title}</Link>
-                </div>
-                <div className="creator">{this.props.element.book.creator}</div>
-
-                <form
-                  onSubmit={e => {
-                    e.preventDefault();
-                    this.props.onOriginUpdate(this.state.description);
-                  }}
-                >
-                  <Textarea
-                    className="form-control description"
-                    name="item-description"
-                    placeholder={'Skriv en beskrivelse'}
-                    onChange={e => this.setState({description: e.target.value})}
-                    value={this.state.description}
-                  />
-                  {this.state.description !== this.props.element.origin && (
-                    <div className="text-right mt1">
-                      <span
-                        className="btn btn-default"
-                        onClick={() => {
-                          this.setState({
-                            description: this.props.element.origin
-                          });
-                        }}
-                      >
-                        <T component="general" name="cancel" />
-                      </span>
-                      <button className="btn btn-success ml2" type="submit">
-                        <T component="general" name="save" />
-                      </button>
-                    </div>
-                  )}
-                </form>
-              </div>
-            </div>
-          </div>
-          <div className="add-to-list col-3">
-            <span
-              className="d-flex align-items-center"
-              onClick={this.props.onAddToList}
-            >
-              <T component="list" name="addToList" />
-              <i className="material-icons" style={{fontSize: 11}}>
-                arrow_forward_ios
-              </i>
-            </span>
-          </div>
-          <div className="order-book col-3">
-            <OrderButton
-              book={this.props.element.book}
-              size="medium"
-              type="tertiary"
-              label="Bestil"
-              className="ml1"
-            >
-              <T component="general" name="bestil" />
-            </OrderButton>
-          </div>
-
+      <div>
+        <div className={`short-list-item d-flex flex-row ${className}`}>
           <i
             className="material-icons remove-btn"
             onClick={this.props.onRemove}
           >
             clear
           </i>
+          <Link href={url}>
+            <BookCover
+              book={work.book}
+              style={{height: 'unset', width: '70px'}}
+            />
+          </Link>
+          <div className="top-bar-dropdown-shortlist-item-page">
+            <Title Tag="h1" type="title5" className="mr-4">
+              {work.book.title}
+            </Title>
+            <Text type="body" className="mb-1">
+              {work.book.creator}
+            </Text>
+            <Text type="body" className="mb-1">
+              {origin}
+            </Text>
+            <Toolbar className="desktop-styling">
+              {addToListButton}
+              <Text align="right" type="body">
+                <T component="work" name="loanTitle" />
+              </Text>
+              {orderBookButton}
+              {orderElectronicBookButtons}
+            </Toolbar>
+            <div className="mobile-styling">{addToListButton}</div>
+          </div>
+        </div>
+        <div className="mobile-styling">
+          <Text align="left" type="body">
+            <T component="work" name="loanTitle" />
+          </Text>
+          <Toolbar className="mobile-styling">
+            <div align="left">
+              {orderBookButton}
+              {orderElectronicBookButtons}
+            </div>
+          </Toolbar>
         </div>
       </div>
     );
   }
 }
-class ShortList extends React.Component {
+
+const ShortListItemWithWork = withWork(ShortListItem, {
+  includeReviews: true,
+  includeCollection: true,
+  includeCover: true
+});
+
+export class ShortList extends React.Component {
   render() {
     const {elements} = this.props.shortListState;
     return (
-      <div className="container">
-        <div className="top-bar-dropdown-list-page col-11 col-centered">
-          <div className="page-header-1">
-            <T component="shortlist" name="title" />
-          </div>
-          <div className="items mb2">
-            <ReactCSSTransitionGroup
-              transitionName="dropdownlist"
-              transitionEnterTimeout={200}
-              transitionLeaveTimeout={200}
-            >
-              {elements.map(e => (
-                <ShortListItem
-                  key={e.book.pid}
-                  element={e}
-                  onRemove={() => {
-                    this.props.remove(e.book.pid);
-                  }}
-                  onOriginUpdate={origin => {
-                    this.props.originUpdate(origin, e.book.pid);
-                  }}
-                  onAddToList={() => {
-                    this.props.addToList(
-                      [
-                        {
-                          book: e.book,
-                          description: e.origin || (
-                            <T component="shortlist" name="origin" />
-                          )
-                        }
-                      ],
-                      this.props.isLoggedIn,
-                      () => this.props.remove(e.book.pid)
-                    );
-                  }}
-                />
-              ))}
-            </ReactCSSTransitionGroup>
-          </div>
-          {elements.length === 0 && (
-            <div className="empty-list-text">
-              <T component="shortlist" name="emptyList" />
+      <React.Fragment>
+        <Banner
+          color="#81c793"
+          className="fixed-width-col-md position-relative text-uppercase"
+          title={T({component: 'shortlist', name: 'title'})}
+        />
+
+        <div className="container">
+          <div className="top-bar-dropdown-list-page col-11 col-centered">
+            <div className="items mb-2">
+              <Toolbar className="top-bar-upper-toolbar">
+                <Button
+                  align="right"
+                  size="large"
+                  type="quaternary"
+                  iconLeft="delete"
+                  className="bg-white pr-0"
+                  onClick={() => this.props.clearList()}
+                >
+                  <T component="shortlist" name="shortlistClear" />
+                </Button>
+              </Toolbar>
+              <Divider />
+              <ReactCSSTransitionGroup
+                transitionName="dropdownlist"
+                transitionEnter={false}
+                transitionLeaveTimeout={200}
+              >
+                {elements.map(e => {
+                  return (
+                    <div key={e.book.pid}>
+                      <ShortListItemWithWork
+                        key={e.book.pid}
+                        origin={e.origin}
+                        pid={e.book.pid}
+                        onRemove={() => {
+                          this.props.remove(e.book.pid);
+                        }}
+                        onOriginUpdate={origin => {
+                          this.props.originUpdate(origin, e.book.pid);
+                        }}
+                        onAddToList={() => {
+                          this.props.addToList(
+                            [
+                              {
+                                book: e.book,
+                                description: e.origin || (
+                                  <T component="shortlist" name="origin" />
+                                )
+                              }
+                            ],
+                            this.props.isLoggedIn,
+                            () => this.props.remove(e.book.pid)
+                          );
+                        }}
+                      />
+                      <Divider />
+                    </div>
+                  );
+                })}
+              </ReactCSSTransitionGroup>
             </div>
-          )}
-          {elements.length > 0 && (
-            <div className="list-actions col-12 col-lg-10 text-right mt4 mb4">
-              <div className="row d-block">
-                <span
-                  className="btn btn-success"
+            {elements.length === 0 && (
+              <div className="empty-list-text">
+                <T component="shortlist" name="emptyList" />
+              </div>
+            )}
+            {elements.length > 0 && (
+              <Toolbar className="bottom-toolbar mt-5 mb-5">
+                <Button
+                  align="right"
+                  iconLeft="list"
+                  size="large"
+                  type="tertiary"
+                  className="text-uppercase"
                   onClick={() =>
                     this.props.addToList(
                       elements,
@@ -172,17 +223,15 @@ class ShortList extends React.Component {
                       this.props.clearList
                     )
                   }
-                  data-cy="listpage-add-elemts-to-list"
                 >
                   <T component="list" name="addAllToList" />
-                </span>
-                <span
-                  className={
-                    'btn ml2 ' +
-                    (this.props.orderList.length > 0
-                      ? 'btn-success'
-                      : 'disabled')
-                  }
+                </Button>
+                <Button
+                  align="right"
+                  iconLeft="chrome_reader_mode"
+                  size="large"
+                  type="quaternary"
+                  className="btn ml-4"
                   onClick={
                     this.props.orderList.length > 0 &&
                     (() =>
@@ -192,21 +241,12 @@ class ShortList extends React.Component {
                   }
                 >
                   <T component="shortlist" name="shortlistOrder" />
-                </span>
-                <span
-                  className="clear-all-btn btn btn-success ml2"
-                  onClick={() => this.props.clearList()}
-                >
-                  <T component="shortlist" name="shortlistClear" />
-                  <i className="material-icons ml1" style={{fontSize: '18px'}}>
-                    delete
-                  </i>
-                </span>
-              </div>
-            </div>
-          )}
+                </Button>
+              </Toolbar>
+            )}
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
