9c0ed45775cc92029cd5f5a7465cae3bd7d00bc8
mtni <mtni@dbc.dk>
2019-04-11 12:36:43 +0200

    replaced width and height 100% in svg

--- src/client/components/general/BookCover.component.js
+++ src/client/components/general/BookCover.component.js
@@ -16,6 +16,8 @@ const generateSvg = (backgroundColor, title, creator) => {
       : '';
 
   return `<svg
+  width="100%"
+  height="100%"
   viewBox="0 0 200 300"
   version="1.1"
   xmlns="http://www.w3.org/2000/svg"
