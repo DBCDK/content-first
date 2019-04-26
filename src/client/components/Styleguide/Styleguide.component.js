f58900f5cb2cd1ae80268bc1ed7e1a15cccdd405
Steen Lillethorup Frederiksen <slf@dbc.dk>
2019-04-26 11:01:54 +0200

    Add disabled buttons to styleguide

--- src/client/components/Styleguide/Styleguide.component.js
+++ src/client/components/Styleguide/Styleguide.component.js
@@ -79,27 +79,43 @@ export default () => (
       <div className="col-8 tl mt2">
         <div>
           <Button type="primary">Primary button</Button>
+          &nbsp;&nbsp;
+          <Button type="primary" disabled>Primary button</Button>
         </div>
         <div className="mt1">
           <Button type="secondary">Secondary button</Button>
+          &nbsp;&nbsp;
+          <Button type="secondary" disabled>Secondary button</Button>
         </div>
         <div className="mt1">
           <Button type="tertiary">Tertiary button</Button>
+          &nbsp;&nbsp;
+          <Button type="tertiary" disabled>Tertiary button</Button>
         </div>
         <div className="mt1">
           <Button type="quaternary">Quaternary button</Button>
+          &nbsp;&nbsp;
+          <Button type="quaternary" disabled>Quaternary button</Button>
         </div>
         <div className="mt1">
           <Button type="quinary">Quinary button</Button>
+          &nbsp;&nbsp;
+          <Button type="quinary" disabled>Quinary button</Button>
         </div>
         <div className="mt1">
           <Button type="term">Term button</Button>
+          &nbsp;&nbsp;
+          <Button type="term" disabled>Term button</Button>
         </div>
         <div className="mt1">
           <Button size="medium">Medium button</Button>
+          &nbsp;&nbsp;
+          <Button size="medium" disabled>Medium button</Button>
         </div>
         <div className="mt1">
           <Button size="small">Small button</Button>
+          &nbsp;&nbsp;
+          <Button size="small" disabled>Small button</Button>
         </div>
         <div className="mt1">
           <Button type="primary" iconLeft="headset">
