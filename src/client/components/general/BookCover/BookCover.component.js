ca77cf13c8d79e5c80ae0d7857e87247af4993ea
Finn Stausgaard <fvs@dbc.dk>
2019-06-12 16:37:38 +0200

    #CF-1260: remove not needed escape

--- src/client/components/general/BookCover/BookCover.component.js
+++ src/client/components/general/BookCover/BookCover.component.js
@@ -36,9 +36,9 @@ const generateSvg = (backgroundColor, title, creator) => {
   </svg>`;
 };
 
-const encodeEntities = (line) => {
-  return line.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
-    return '&#'+i.charCodeAt(0)+';';
+const encodeEntities = line => {
+  return line.replace(/[\u00A0-\u9999<>&]/gim, function(i) {
+    return '&#' + i.charCodeAt(0) + ';';
   });
 };
 
