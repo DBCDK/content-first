3e962e4870ad5d674ed4aa6aa30819a336c10859
Yousef Mohsen <yousefmohsen@outlook.dk>
2019-07-22 11:24:57 +0200

    withList hoc in printLayout

--- src/client/components/list/printLayout/PrintLayout.js
+++ src/client/components/list/printLayout/PrintLayout.js
@@ -1,13 +1,13 @@
-/** Provide with an array of pid's. E.g. list=['pid1','pid2']
- or an list id/'huskeliste' in the id prop. E.g. id='huskeliste' or id='65f4a372-bbd1-4c37-b806-5716bfbd9cf1' */
+/** Provide with an array of pid's. E.g. printList={[{pid:'870970-basis:46647181'},{pid:'870970-basis:29572178'}]}
+ alternativly you can provide an list id/'huskeliste' in the id prop. E.g. id='huskeliste' or id='65f4a372-bbd1-4c37-b806-5716bfbd9cf1' */
 import React from 'react';
 import PropTypes from 'prop-types';
-import {loadList} from '../../../utils/requestLists';
 import Spinner from '../../general/Spinner/Spinner.component';
 import PrintElement from './PrintElement';
 import Title from '../../base/Title';
 import Text from '../../base/Text';
 import {connect} from 'react-redux';
+import {withList} from '../../hoc/List';
 
 import './PrintLayout.css';
 
@@ -15,48 +15,44 @@ class PrintLayout extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      list: this.props.list,
+      list: this.props.printList,
       title: this.props.title,
-      description: this.props.description
+      description: this.props.description,
+      didShowPrintModal: false
     };
   }
-  componentDidMount = async () => {
-    if (!this.state.list || (this.state.list && this.state.list.length === 0)) {
-      this.getList();
-    }
-  };
-  componentDidUpdate = async () => {
-    if (this.state.list && this.state.list.length === 0) {
-      this.getList();
-    }
-  };
+  componentDidMount() {
+    this.loadShortList();
+  }
 
-  getList = async () => {
-    try {
-      const id = this.props.id;
-      const isShortList = id === 'huskeliste';
-      let newState = {};
-      if (isShortList) {
-        const {elements} = this.props.shortListState;
-
-        newState = {
-          title: 'Huskeliste',
-          list: elements
-          //  description: "Huskeliste fra Læsekompas.dk"
-        };
-      } else {
-        const loadedList = await loadList(id);
-        newState = {
-          list: loadedList.list,
-          title: loadedList.title,
-          description: loadedList.description
-        };
-      }
-      this.setState(newState);
-    } catch (error) {
-      // nothing to handle
+  componentDidUpdate(prevProps) {
+    if (prevProps.list !== this.props.list && this.props.list.title) {
+      const loadedList = this.props.list;
+      this.setState({
+        list: loadedList.list,
+        title: loadedList.title,
+        description: loadedList.description
+      });
+    } else if (
+      this.state.list &&
+      this.state.list.length === 0 &&
+      this.props.shortListState.elements.length > 0
+    ) {
+      this.loadShortList();
     }
-  };
+  }
+
+  loadShortList() {
+    const isShortList = this.props.id === 'huskeliste';
+    if (isShortList) {
+      const {elements} = this.props.shortListState;
+      const shortlist = elements.map(el => (el.pid ? el : el.book));
+      this.setState({
+        title: 'Huskeliste',
+        list: shortlist
+      });
+    }
+  }
 
   render() {
     let list = this.state.list;
@@ -94,11 +90,12 @@ PrintLayout.propTypes = {
   id: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
-  list: PropTypes.array
+  list: PropTypes.object,
+  printList: PropTypes.array
 };
 const mapStateToProps = state => {
   return {
     shortListState: state.shortListReducer
   };
 };
-export default connect(mapStateToProps)(PrintLayout);
+export default connect(mapStateToProps)(withList(PrintLayout));
