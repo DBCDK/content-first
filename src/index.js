eecfd4ef1df283a9712dd74c24b3c24e5f3a1c7b
Yousef Mohsen <yousefmohsen@outlook.dk>
2019-08-02 13:51:32 +0200

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import 'core-js';
-import 'regenerator-runtime/runtime';
-import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
 import smoothscroll from 'smoothscroll-polyfill';
 import './prototypes';
 
