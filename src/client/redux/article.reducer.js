af97e2fd1d8b48daf97a188bba113c0957ade91c
mtni <mtni@dbc.dk>
2019-08-22 12:17:23 +0200

    implemented loginerror page

--- src/client/redux/article.reducer.js
+++ src/client/redux/article.reducer.js
@@ -1,5 +1,6 @@
 /* eslint import/no-webpack-loader-syntax: off */
 import About from '!raw-loader!../components/article/pages/about.md';
+import LoginError from '!raw-loader!../components/article/pages/loginError.md';
 import NotFound from '!raw-loader!../components/article/pages/404.md';
 
 const defaultState = {
@@ -22,8 +23,18 @@ const defaultState = {
       },
       src: About
     },
-    '/404': {
+    '/loginfejl': {
       id: 2,
+      name: 'loginfejl',
+      meta: {
+        title: 'Kompasset blev forvirret',
+        canonical: '/loginfejl',
+        robots: 'noindex'
+      },
+      src: LoginError
+    },
+    '/404': {
+      id: 3,
       name: '404',
       meta: {
         title: '404 - Siden blev ikke fundet',
