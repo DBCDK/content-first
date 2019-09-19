bdf0df52564da34cddd4f761aaa3c40c98c6ae6c
mtni <mtni@dbc.dk>
2019-09-19 09:11:56 +0200

    fixed pin

---
+++ src/shared/belts.requester.js
@@ -0,0 +1,16 @@
+import {findIndex} from 'lodash';
+
+export default class BeltsRequester {
+  constructor({storageClient}) {
+    this.storageClient = storageClient;
+  }
+
+  async fetchOwnedBelts(owner) {
+    const belts = (await this.storageClient.find({
+      type: 'belt',
+      owner
+    })).data;
+
+    return belts;
+  }
+}
