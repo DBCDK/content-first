941b9616f985b73b9a152ff7824a236b9e09d3cd
Kasper F <kfu@dbc.dk>
2019-11-06 14:05:51 +0100

    kiosk hero

--- src/client/components/kiosk/KioskHero.component.js
+++ src/client/components/kiosk/KioskHero.component.js
@@ -4,37 +4,60 @@ import './KioskHero.css';
 import Title from '../base/Title';
 import Button from '../base/Button';
 import Icon from '../base/Icon';
+import scroll from '../../utils/scroll';
+import T from '../base/T';
 
 import BookSearchSuggester from '../list/addtolist/BookSearchSuggester';
 
 const RoundLabel = () => {
   return (
-    <div className="RoundLabel">
+    <div
+      className="RoundLabel"
+      onClick={() => {
+        scroll(1096);
+      }}
+    >
       <div>Eller få inspiration her</div>
       <Icon name="keyboard_arrow_down" />
     </div>
   );
 };
 
-export default () => (
-  <div className="KioskHero">
-    <Title type="title1">Find din næste roman</Title>
-    <Title type="title2" variant="color-white">
-      Start med en god bog du har læst
-    </Title>
-    <div className="Search">
-      <BookSearchSuggester
-        // suggesterRef={suggesterRef}
-        className="ml-3 mr-5"
-        // list={list}
-        onSubmit={e => {
-          console.log(e);
-        }}
-      />
-      <Button type="primary">Søg</Button>
-    </div>
-    <div className="RoundLabelWrapper">
-      <RoundLabel />
-    </div>
-  </div>
-);
+export default class KioskHero extends React.Component {
+  state = {inputFocused: false};
+  render() {
+    return (
+      <div
+        className={`KioskHero ${
+          this.state.inputFocused ? 'input-focused' : ''
+        }`}
+      >
+        <Title type="title1">
+          <T component="kioskHero" name="title" />
+        </Title>
+        <Title type="title2" variant="color-white">
+          <T component="kioskHero" name="subtitle" />
+        </Title>
+        <div className="Search">
+          <BookSearchSuggester
+            onSubmit={e => {}}
+            onFocus={() => {
+              this.setState({inputFocused: true});
+              scroll(0);
+            }}
+            onBlur={() => {
+              this.setState({inputFocused: false});
+            }}
+            placeholder={T({component: 'kioskHero', name: 'placeholder'})}
+          />
+          <Button type="primary">
+            <T component="kioskHero" name="searchBtn" />
+          </Button>
+        </div>
+        <div className="RoundLabelWrapper">
+          <RoundLabel />
+        </div>
+      </div>
+    );
+  }
+}
