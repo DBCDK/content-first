468752523ec3f87726a6724a1683a4ba8b4570e4
Kasper F <kfu@dbc.dk>
2019-11-21 14:18:18 +0100

    only render relevant components wrapped by withHoldings

--- src/client/components/hoc/Holding/withHoldings.hoc.js
+++ src/client/components/hoc/Holding/withHoldings.hoc.js
@@ -9,11 +9,11 @@ import {fetchHoldings} from '../../../redux/holdings.thunk';
  * @param {object} pid - The pid for the material to fetch Holdings for
  * @return {object} - returns the Holdings
  **/
-
 export const withHoldings = WrappedComponent => props => {
   const {agencyId, branch, pid} = props;
   const [hasDispatched, setHasDispatched] = useState(false);
-  const {holdings} = useSelector(store => store.holdings, shallowEqual);
+
+  const holdings = useSelector(store => store.holdings.holdings[pid]);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,7 +23,7 @@ export const withHoldings = WrappedComponent => props => {
     }
   }, [agencyId, branch, pid, dispatch, hasDispatched]);
 
-  return <WrappedComponent pid={pid} holdings={holdings[pid]} />;
+  return <WrappedComponent pid={pid} holdings={holdings} />;
 };
 
 export default withHoldings;
