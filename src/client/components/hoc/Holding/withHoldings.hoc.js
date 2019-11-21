7393fcbc46a2715074597ac7fcc722cf988daafb
Kasper F <kfu@dbc.dk>
2019-11-21 14:28:20 +0100

    fix lint

--- src/client/components/hoc/Holding/withHoldings.hoc.js
+++ src/client/components/hoc/Holding/withHoldings.hoc.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {useDispatch, useSelector, shallowEqual} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {fetchHoldings} from '../../../redux/holdings.thunk';
 
 /**
