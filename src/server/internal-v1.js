413304f20c0a6267f8b789f60746a0caa123d3b5
Steen Lillethorup Frederiksen <slf@dbc.dk>
2020-01-10 15:33:44 +0100

    Remove /v1/image from internal

--- src/server/internal-v1.js
+++ src/server/internal-v1.js
@@ -7,7 +7,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/image', require('server/internal-v1-image'));
 router.use('/book', require('server/internal-v1-book'));
 router.use('/books', require('server/internal-v1-books'));
 router.use('/tags', require('server/internal-v1-tags'));
