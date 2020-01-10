37b0bdffffd088517ae266118472bf756854e847
mtni <36037828+mtni@users.noreply.github.com>
2020-01-10 10:11:17 +0100

    Kiosk hotjar (#1252)
    * hotjar enable d in kioskmode
    
    * updated snapshot test
    
    * changed hotjar code

--- src/client/components/base/Head/Head.js
+++ src/client/components/base/Head/Head.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import {Helmet} from 'react-helmet';
+import {get} from 'lodash';
 
 function buildOGMeta(obj) {
   return Object.keys(obj).map((k, i) => {
@@ -37,7 +39,6 @@ function buildOGMeta(obj) {
       }
     }}
   />
-
 */
 
 const Head = ({
@@ -56,6 +57,12 @@ const Head = ({
     book: {}
   }
 }) => {
+  // isKiosk
+  const isKiosk = useSelector(state => get(state, 'kiosk.enabled', false));
+
+  // hotjar tracking code
+  const hotjar_id = isKiosk ? 1636409 : 1361823;
+
   return (
     <Helmet>
       <title>{title === 'Læsekompas' ? title : `${title} | Læsekompas`}</title>
@@ -67,6 +74,17 @@ const Head = ({
       {(og && buildOGMeta(og)) || null}
       {(og && og.image && buildOGMeta(og.image)) || null}
       {(og && og.book && buildOGMeta(og.book)) || null}
+
+      <script>
+        {`(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${hotjar_id},hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </script>
     </Helmet>
   );
 };
