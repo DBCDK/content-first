795193e86cb674d08d3485fe5028fa21f95cb1ea
Stanford Vinson <stvi@dbc.dk>
2020-03-23 11:37:19 +0100

    added duplicate check to mounts reducer

--- src/client/redux/mounts.reducer.js
+++ src/client/redux/mounts.reducer.js
@@ -3,20 +3,28 @@ const defaultState = {};
 export const UPDATE_MOUNT = 'UPDATE_MOUNT';
 
 const mountsReducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case UPDATE_MOUNT: {
-      const mounts = {...state};
-      mounts[action.mount] = {
-        ...(mounts[action.mount] || {}),
-        ...action.data
-      };
-
-      return mounts;
+    switch (action.type) {
+      case UPDATE_MOUNT: {
+        const mounts = {...state};
+        mounts[action.mount] = {
+          ...(mounts[action.mount] || {}),
+          ...action.data
+        };
+        let parent;
+        Object.keys(mounts).forEach((key)=> {
+          if (mounts[key].type === 'SIMILAR') {
+            if (mounts[key].parent === parent) {
+              delete mounts[key];
+            } else {
+              parent = mounts[key].parent;
+            }
+          }
+        });
+        return mounts;
+      }
+      default:
+        return state;
     }
-
-    default:
-      return state;
-  }
-};
+  };
 
 export default mountsReducer;
