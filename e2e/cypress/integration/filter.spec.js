313a695bc26852b25386a0c1cdbd1e8835506446
mtni <mtni@dbc.dk>
2020-04-06 18:01:58 +0200

    ...,

--- e2e/cypress/integration/filter.spec.js
+++ e2e/cypress/integration/filter.spec.js
@@ -126,7 +126,7 @@ describe('Filter page test', function() {
     cy.get('[data-cy=filterDimmer]').click();
     cy.reload(); // Because VisibilitySensor does not detect changes under Cypress
 
-    cy.get('[data-cy=container-row]:first > [data-cy=workcard-1]').click('top');
+    cy.get('[data-cy=container-row]:first > [data-cy=workcard-0]').click('top');
     cy.get('.work-preview [data-cy=book-cover-loaded]')
       .first()
       .click();
